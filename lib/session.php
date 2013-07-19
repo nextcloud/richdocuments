2a7b9a6e1ba9d1f63c19bea80c06e0b569ef6d00
Victor Dubiniuk <victor.dubiniuk@gmail.com>
2013-07-19 20:44:18 +0300

    Session reading. Mock session

--- lib/session.php
+++ lib/session.php
@@ -3,5 +3,19 @@
 namespace OCA\Office;
 
 class Session {
-	//put your code here
+	public static function getSession($id){
+		$query = \OCP\DB::prepare('SELECT * FROM `*PREFIX*office_session` WHERE `es_id`= ?');
+		$result = $query->execute(array($id));
+		return $result->fetchRow();
+	}
+	
+	public function setMockSession(){
+		$query = \OCP\DB::prepare('INSERT INTO `*PREFIX*office_session`  (`es_id`, `genesis_url`, `genesis_hash`, `owner`) VALUES (?, ?, ?, ?) ');
+		$result = $query->execute(array(
+			'dev0',
+			'/welcome.odt',
+			'0xdeadbeef',
+			\OCP\User::getUser()
+		));
+	}
 }
