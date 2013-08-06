34c8ab53dc20b70f840281e8a94cf75b59c1bae6
Victor Dubiniuk <victor.dubiniuk@gmail.com>
2013-08-07 00:48:43 +0300

    Make seq autoincrement

--- lib/op.php
+++ lib/op.php
@@ -5,10 +5,9 @@ namespace OCA\Office;
 class Op {
 
 	public static function add($op){
-		$query = \OCP\DB::prepare('INSERT INTO `*PREFIX*office_op`  (`es_id`, `seq`, `member`, `opspec`) VALUES (?, ?, ?, ?) ');
+		$query = \OCP\DB::prepare('INSERT INTO `*PREFIX*office_op`  (`es_id`, `member`, `opspec`) VALUES (?, ?, ?) ');
 		$result = $query->execute(array(
 			$op['es_id'],
-			$op['seq'],
 			$op['member'],
 			$op['opspec'],
 		));
