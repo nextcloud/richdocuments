243c6c56d2640faf7697631c5f0285bbd46626fe
Tobias Hintze <th-git@kogmbh.com>
2013-08-18 11:07:35 +0200

    add a string to the file_id-link after file_id change

--- templates/part.sessions.php
+++ templates/part.sessions.php
@@ -2,5 +2,5 @@
 <div><a href="" data-esid="<?php p($invite['es_id']); ?>"><?php p($invite['es_id']); ?></a></div>
 <?php } ?>
 <?php foreach ($_['sessions'] as $session){ ?>
-<div><a href="" data-esid="<?php p($session['es_id']); ?>"><?php p($session['document_path']); ?></a></div>
+<div><a href="" data-esid="<?php p($session['es_id']); ?>">Session: <?php p($session['file_id']); ?></a></div>
 <?php } ?>
