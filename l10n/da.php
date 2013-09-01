9c0a0c854aee68eb97c9793ef3260484257473ea
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2013-09-01 11:40:51 -0400

    [tx-robot] updated from transifex

--- l10n/da.php
+++ l10n/da.php
@@ -1,4 +1,15 @@
 <?php $TRANSLATIONS = array(
 "Close" => "Luk",
-"No people found" => "Ingen personer fundet"
+"Invite" => "Inviter",
+"Users in session:" => "Brugere i møde:",
+"Failed to load documents." => "Kunne ikke hente dokumenterne. ",
+"No people found" => "Ingen personer fundet",
+"Remove from the list" => "Fjern fra listen",
+"Sent" => "Sent",
+"Declined" => "Afvist",
+"Accepted" => "Accepteret",
+"Incoming" => "Indgående",
+"Documents" => "Dokumenter",
+"Advanced feature-set" => "Avancerede funktioner",
+"(Unstable)" => "(Ustabil)"
 );
--- l10n/zh_TW.php
+++ l10n/zh_TW.php
@@ -1,4 +1,15 @@
 <?php $TRANSLATIONS = array(
 "Close" => " 關閉",
-"No people found" => "沒有找到任何人"
+"Invite" => "邀請",
+"Users in session:" => "上線的使用者：",
+"Failed to load documents." => "載入文件失敗",
+"No people found" => "沒有找到任何人",
+"Remove from the list" => "從清單移除",
+"Sent" => "已送出",
+"Declined" => "已拒絕",
+"Accepted" => "以接受",
+"Incoming" => "新進的",
+"Documents" => "文件",
+"Advanced feature-set" => "進階功能",
+"(Unstable)" => "（不穩定）"
 );
