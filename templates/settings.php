504dc9c81e50d911934c1f72ef69d999f1b90129
Jan-Christoph Borchardt <hey@jancborchardt.net>
2013-10-05 13:50:33 +0300

    more elegant style for Personal and Admin settings, also for Documents

--- templates/settings.php
+++ templates/settings.php
@@ -1,18 +1,12 @@
 <fieldset class="personalblock" id="documents">
-	<legend><strong><?php p($l->t('Documents')) ?></strong></legend>
-	<table class="nostyle">
-		<tbody><tr>
-				<td>
-					<input id="webodf-unstable" type ="checkbox"
-						   <?php if ($_['unstable'] === 'true'){ ?>
-							   checked="checked"
-						   <?php } ?>
-						   />
-					<label for="webodf-unstable">
-						<?php p($l->t('Advanced feature-set'))?>&nbsp;
-						<?php p($l->t('(Unstable)')); ?>
-					</label>
-				</td>
-			</tr>
-		</tbody></table>
+	<h2><?php p($l->t('Documents')) ?></h2>
+	<input id="webodf-unstable" type ="checkbox"
+		<?php if ($_['unstable'] === 'true'){ ?>
+			checked="checked"
+		<?php } ?>
+	/>
+	<label for="webodf-unstable">
+		<?php p($l->t('Advanced feature-set'))?>&nbsp;
+		<?php p($l->t('(Unstable)')); ?>
+	</label>
 </fieldset>
