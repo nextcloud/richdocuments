f4f11678d8f7eca8ea4daf259f6c341a5a45c76f
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2013-10-08 07:14:30 -0400

    [tx-robot] updated from transifex

--- l10n/mk.php
+++ l10n/mk.php
@@ -1,5 +1,20 @@
 <?php $TRANSLATIONS = array(
 "Close" => "Затвори",
 "Share" => "Сподели",
-"Upload" => "Подигни"
+"Leaving this page in Editor mode might cause unsaved data. It is recommended to use 'Close' button instead." => "Оставањето на оваа страница во мод за уредување може да предизивка губиток на неснимените податоци. Се препорачува да се користи копчето 'Затвори'.",
+"Failed to load this document. Please check if it can be opened with an external odt editor. This might also mean it has been unshared or deleted recently." => "Неуспешно вчитување на документот. Ве молиме да проверите дали може да се отвори со надворешен odt уредувач. Ова исто така може да значи дека истиот бил одстранет од споделување или пак претходно бил избришан.",
+"Failed to load documents." => "Не успеав да го вчитам документот",
+"No documents are found. Please upload or create a document!" => "Не се пронајдени документи. Ве молиме ставете или креирајте документ!",
+"Sent" => "Испратен",
+"Declined" => "Одбиен",
+"Accepted" => "Прифатен",
+"Incoming" => "Надоаѓачки",
+"Upload" => "Подигни",
+"Documents" => "Документи",
+"Save new documents to" => "Сними го новиот документ во",
+"Introduce yourself" => "Представи се",
+"Join" => "Спои",
+"This link has been expired or is never existed. Please contact the person who shared it with you for details." => "Овој линк истекол или никогаш не постоел. Ве молиме контактирајте го корисникот кој го споделил линкот со вас за повеќе детали.",
+"Advanced feature-set" => "Напредни опции",
+"(Unstable)" => "(Нестабилен)"
 );
