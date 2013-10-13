a4194a643f470627d9b7ba11a966aa625a1ef469
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2013-10-13 18:26:08 -0400

    [tx-robot] updated from transifex

--- l10n/nl.php
+++ l10n/nl.php
@@ -1,13 +1,20 @@
 <?php $TRANSLATIONS = array(
 "Close" => "Sluiten",
 "Share" => "Delen",
+"Leaving this page in Editor mode might cause unsaved data. It is recommended to use 'Close' button instead." => "Deze pagina open laten in de Editor kan resulteren in niet opslaan. Geadviseerd wordt om de 'Sluiten' knop te gebruiken.",
+"Failed to load this document. Please check if it can be opened with an external odt editor. This might also mean it has been unshared or deleted recently." => "Kon dit document niet laden. Ga na of het met een externe odt editor wel kan worden geopend. Het kan zijn dat het niet meer is gedeeld of dat het recent is verwijderd.",
 "Failed to load documents." => "Kon documenten niet laden.",
+"No documents are found. Please upload or create a document!" => "Geen documenten gevonden. Upload of maak een document!",
 "Sent" => "Verzonden",
 "Declined" => "Afgewezen",
 "Accepted" => "Geaccepteerd",
 "Incoming" => "Inkomend",
 "Upload" => "Uploaden",
 "Documents" => "Documenten",
+"Save new documents to" => "Bewaar nieuwe documenten  in",
+"Introduce yourself" => "Introduceer uzelf",
+"Join" => "Meedoen",
+"This link has been expired or is never existed. Please contact the person who shared it with you for details." => "De link is vervallen of heeft nooit bestaan. Neem contact op met de persoon die het deelde voor de details.",
 "Advanced feature-set" => "Geavanceerde functies",
 "(Unstable)" => "(Onstabiel)"
 );
