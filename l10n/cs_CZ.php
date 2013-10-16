cdbba60926e69c93cd3a3736bc73d6024cf44003
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2013-10-15 21:30:00 -0400

    [tx-robot] updated from transifex

--- l10n/cs_CZ.php
+++ l10n/cs_CZ.php
@@ -1,13 +1,20 @@
 <?php $TRANSLATIONS = array(
 "Close" => "Zavřít",
 "Share" => "Sdílet",
+"Leaving this page in Editor mode might cause unsaved data. It is recommended to use 'Close' button instead." => "Ponecháním této stránky v módu editoru může dojít k neuložení změn. Místo toho je doporučeno použít tlačítko 'Zavřít'.",
+"Failed to load this document. Please check if it can be opened with an external odt editor. This might also mean it has been unshared or deleted recently." => "Nepodařilo se otevřít tento dokument. Prosím zkontrolujte, zda je možné jej otevřít externím editorem odt. Může to také znamenat, že byl nedávno odsdílen nebo smazán.",
 "Failed to load documents." => "Nepodařilo se načíst dokumenty.",
+"No documents are found. Please upload or create a document!" => "Žádné dokumenty nebyly nalezeny. Nahrajte nebo vytvořte dokument!",
 "Sent" => "Odesláno",
 "Declined" => "Zamítnuto",
 "Accepted" => "Schváleno",
 "Incoming" => "Příchozí",
 "Upload" => "Odeslat",
 "Documents" => "Dokumenty",
+"Save new documents to" => "Uložit nové dokumenty do",
+"Introduce yourself" => "Představte se",
+"Join" => "Vstoupit",
+"This link has been expired or is never existed. Please contact the person who shared it with you for details." => "Tento odkaz expiroval nebo neexistuje. Pro podrobnosti kontaktujte osobu, která jej s vámi sdílela.",
 "Advanced feature-set" => "Pokročilá sada možností",
 "(Unstable)" => "(Nestabilní)"
 );
--- l10n/tr.php
+++ l10n/tr.php
@@ -1,13 +1,20 @@
 <?php $TRANSLATIONS = array(
 "Close" => "Kapat",
 "Share" => "Paylaş",
+"Leaving this page in Editor mode might cause unsaved data. It is recommended to use 'Close' button instead." => "Düzenleyici kipinde bu sayfadan ayrılmak kaydedilmemiş veriye sebep olabilir. Bunun yerine 'Kapat' düğmesi kullanılması önerilir.",
+"Failed to load this document. Please check if it can be opened with an external odt editor. This might also mean it has been unshared or deleted recently." => "Bu belge yüklenemedi. Lütfen harici bir odt düzenleyicisi ile açılıp açılamayacağını denetleyin. Bu ayrıca paylaşımın yapılmadığı veya yakın zamanda silinmiş olabileceğinden de olabilir.",
 "Failed to load documents." => "Belgeler yüklenemedi.",
+"No documents are found. Please upload or create a document!" => "Hiçbir belge bulunamadı. Lütfen bir belge yükleyin veya oluşturun!",
 "Sent" => "Gönderildi",
 "Declined" => "Reddedildi",
 "Accepted" => "Kabul edildi",
 "Incoming" => "Gelen",
 "Upload" => "Yükle",
 "Documents" => "Belgeler",
+"Save new documents to" => "Yeni belgeleri şuraya kaydet",
+"Introduce yourself" => "Kendinizi tanıtın",
+"Join" => "Katıl",
+"This link has been expired or is never existed. Please contact the person who shared it with you for details." => "Bu bağlantının süresi dolmuş veya hiç mevcut olmamış. Lütfen ayrıntılar için paylaşımı yapan kişi ile iletişime geçin.",
 "Advanced feature-set" => "Gelişmiş özellik seti",
 "(Unstable)" => "(Kararsız)"
 );
