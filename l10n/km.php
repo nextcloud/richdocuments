0d7f16bb3ea4e6480f9a68fa2ecb6236a788c411
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2014-02-25 00:44:09 -0500

    [tx-robot] updated from transifex

--- l10n/km.php
+++ l10n/km.php
@@ -1,3 +1,9 @@
 <?php $TRANSLATIONS = array(
-"Delete" => "លុប"
+"Close" => "បិទ",
+"Share" => "ចែក​រំលែក",
+"Cancel" => "លើកលែង",
+"Delete" => "លុប",
+"Save" => "រក្សាទុក",
+"Edit" => "កែ",
+"Password" => "ពាក្យសម្ងាត់"
 );
