c9a7bff08213183e13ce9d0bf0d6c30f8d3f79c4
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-08-17 00:43:24 -0400

    [tx-robot] updated from transifex

---
+++ l10n/az.php
@@ -0,0 +1,15 @@
+<?php $TRANSLATIONS = array(
+"Format filter server is down or misconfigured" => "Fomat filtrasiyası serveri sönülüdür yada düzgün config edilməyib",
+"Conversion failed. Check log for details." => "Dəyişiklikdə səh oldu. Ətraflı məlumat üçün jurnal faylını yoxlayın",
+"Saved" => "Saxlanıldı",
+"Can't create document" => "Sənədi yaratmaq mümkün olmadı",
+"You don't have permission to rename this document" => "Bu sənədin adını dəyişmək üçün sizin yetkiniz yoxdur.",
+"An error occurred while changing directory." => "Qovluğu dəyişdikdə səhv baş verdi.",
+"Saving..." => "Saxlama...",
+"Share" => "Yayımla",
+"Save" => "Saxlamaq",
+"Cancel" => "Dayandır",
+"Close" => "Bağla",
+"Size" => "Həcm",
+"Upload" => "Serverə yüklə"
+);
