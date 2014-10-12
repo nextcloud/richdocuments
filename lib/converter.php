6ae84ff682c1fb1605387ef5f0aa7f01d181514f
aaukt <aaukt@users.noreply.github.com>
2014-10-13 00:09:44 +0200

    fix target format for external format-filter-server
    According to https://github.com/owncloud/format-filter-server/blob/cc435eac37fb6e67f73fb47d404281c59aa49bd7/code/conversion.js#L17 the target_format should be "odt".
    
    The format-filter-server returns an empty response otherwise.

--- lib/converter.php
+++ lib/converter.php
@@ -30,7 +30,7 @@ class Converter {
 	
 	public static function checkConnection(){
 		$expected = file_get_contents(__DIR__ . '/response.odt');
-		$converted = self::convertExternal('', 'application/vnd.oasis.opendocument.text');
+		$converted = self::convertExternal('', 'odt');
 		
 		return $converted === $expected;
 	}
