4330383b9559c570f2afdf86064aafe02a938138
Victor Dubiniuk <victor.dubiniuk@gmail.com>
2014-10-29 15:39:10 +0300

    Add generic test

---
+++ tests/controller/documentcontrollertest.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * ownCloud - Documents App
+ *
+ * @author Victor Dubiniuk
+ * @copyright 2014 Victor Dubiniuk victor.dubiniuk@gmail.com
+ *
+ * This file is licensed under the Affero General Public License version 3 or
+ * later.
+ */
+
+namespace OCA\Documents\Controller;
+
+class DocumentControllerTest extends \PHPUnit_Framework_TestCase {
+	private $appName;
+	private $request;
+	private $l10n;
+	private $settings;
+	private $uid;
+	private $password;
+	private $controller;
+	
+	public function setUp(){
+		$this->appName = 'documents';
+		$this->uid = 'jack_the_documents_tester';
+		$this->password = 'password';
+		$this->request = $this->getMockBuilder('\OCP\IRequest')
+			->disableOriginalConstructor()
+			->getMock()
+		;	
+		$this->settings = $this->getMockBuilder('\OCP\IConfig')
+			->disableOriginalConstructor()
+			->getMock()
+		;
+		$this->l10n = $this->getMockBuilder('\OCP\IL10N')
+			->disableOriginalConstructor()
+			->getMock()
+		;
+		$this->controller = new DocumentController(
+			$this->appName,
+			$this->request,
+			$this->settings,
+			$this->l10n,
+			$this->uid
+		);
+	}
+	
+	public function tearDown(){
+		\OC_User::deleteUser($this->uid);
+	}
+	
+	public function testRename(){
+		$this->login();
+		$result = array(
+			'status' => 'error',
+			'message' => (string) $this->l10n->t('You don\'t have permission to rename this document')
+		);
+		$this->request->post = array(
+			'fileId' => 500,
+			'name' => 'newname.ext'
+		);
+		$response = $this->controller->rename(500);
+		$this->assertEquals($result, $response);
+	}
+	
+	protected function login(){
+		if (!\OCP\User::userExists($this->uid)){
+			\OC_User::createUser($this->uid, $this->password);
+		}
+		if (!\OC_User::isLoggedIn()){
+			$result = \OC_User::getUserSession()->login($this->uid, $this->password);
+			if ($result){
+				\OC_Util::setupFS(\OC_User::getUserSession()->getUser()->getUID());
+			}
+		}
+	}
+}
