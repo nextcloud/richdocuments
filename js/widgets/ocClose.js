82122ecb2bda8373c9f45e8bd58f20e711fea714
Jan-Christoph Borchardt <hey@jancborchardt.net>
2014-12-23 15:16:14 +0100

    better style for close button

--- js/widgets/ocClose.js
+++ js/widgets/ocClose.js
@@ -5,8 +5,8 @@ define("owncloud/widgets/ocClose",
 			"use strict";
 			return declare("OcClose", [_WidgetBase, _TemplatedMixin], {
 				templateString: '<span class="dijit">' +
-						'<button id="odf-close" class="icon-close svg" data-dojo-attach-event="onclick: closeEditor"></button>' +
-						'<img id="saving-document" alt="" src="' + OC.imagePath('core', 'loading.gif') + '" /></span>',
+						'<button id="odf-close" class="icon-view-close svg" data-dojo-attach-event="onclick: closeEditor"></button>' +
+						'<img id="saving-document" alt="" src="' + OC.imagePath('core', 'loading-dark.gif') + '" /></span>',
 				closeEditor: function () {
 					// FixMe: this should not be global
 					documentsMain.onClose();
