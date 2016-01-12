d290e21b401ce61faa0a71b8661859860f3f2be8
Andras Timar <andras.timar@collabora.com>
2016-01-12 15:20:38 +0100

    remove trailing whitespaces

--- controller/documentcontroller.php
+++ controller/documentcontroller.php
@@ -29,21 +29,21 @@ use OCA\Richdocuments\Genesis;
 use \OC\Files\View;
 
 class DocumentController extends Controller{
-	
+
 	private $uid;
 	private $l10n;
 	private $settings;
-	
+
 	const ODT_TEMPLATE_PATH = '/assets/odttemplate.odt';
 	const CLOUDSUITE_TMP_PATH = '/documents-tmp/';
-	
+
 	public function __construct($appName, IRequest $request, IConfig $settings, IL10N $l10n, $uid){
 		parent::__construct($appName, $request);
 		$this->uid = $uid;
 		$this->l10n = $l10n;
 		$this->settings = $settings;
 	}
-	
+
 	/**
 	 * @NoAdminRequired
 	 * @NoCSRFRequired
@@ -72,7 +72,7 @@ class DocumentController extends Controller{
 
 		return $response;
 	}
-	
+
 	/**
 	 * @NoAdminRequired
 	 */
@@ -100,17 +100,17 @@ class DocumentController extends Controller{
 		}
 
 		$path = Helper::getNewFileName($view, $dir . '/' . $basename);
-		
+
 		$content = '';
 		if (class_exists('\OC\Files\Type\TemplateManager')){
 			$manager = \OC_Helper::getFileTemplateManager();
 			$content = $manager->getTemplate($mimetype);
 		}
-		
+
 		if (!$content){
 			$content = file_get_contents(dirname(__DIR__) . self::ODT_TEMPLATE_PATH);
 		}
-		
+
 		if ($content && $view->file_put_contents($path, $content)){
 			$info = $view->getFileInfo($path);
 			$response =  array(
@@ -222,11 +222,11 @@ class DocumentController extends Controller{
 	public function serve($esId){
 		$session = new Db\Session();
 		$session->load($esId);
-		
+
 		$filename = $session->getGenesisUrl() ? $session->getGenesisUrl() : '';
 		return new DownloadResponse($this->request, $session->getOwner(), $filename);
 	}
-	
+
 	/**
 	 * @NoAdminRequired
 	 */
@@ -236,7 +236,7 @@ class DocumentController extends Controller{
 			$response->setStatus(Http::STATUS_BAD_REQUEST);
 			return $response;
 		}
-		
+
 		$fullPath = '/files' . $path;
 		$fileInfo = \OC\Files\Filesystem::getFileInfo($path);
 		if ($fileInfo){
@@ -248,7 +248,7 @@ class DocumentController extends Controller{
 		}
 		return new DownloadResponse($this->request, $this->uid, $fullPath);
 	}
-	
+
 
 	/**
 	 * @NoAdminRequired
@@ -295,13 +295,13 @@ class DocumentController extends Controller{
 		usort($documents, function($a, $b){
 			return @$b['mtime']-@$a['mtime'];
 		});
-		
+
 		$session = new Db\Session();
 		$sessions = $session->getCollectionBy('file_id', $fileIds);
 
 		$members = array();
 		$member = new Db\Member();
-		foreach ($sessions as $session) {			
+		foreach ($sessions as $session) {
 			$members[$session['es_id']] = $member->getActiveCollection($session['es_id']);
 		}
 
