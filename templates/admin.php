945e9c5e06fc88f1c51e361764eac80be7406f1e
Jan Holesovsky <kendy@collabora.com>
2016-04-12 11:39:16 +0200

    Better wording of the WOPI configuration.

--- templates/admin.php
+++ templates/admin.php
@@ -3,10 +3,9 @@ script('richdocuments', 'admin');
 ?>
 <div class="section" id="richdocuments">
 	<h2><?php p($l->t('Collabora Online')) ?></h2>
-	<div id="wopi_client">
-		<input type="text" name="wopi_url" id="wopi_url" value="<?php p($_['wopi_url'])?>" style="width:250px;">
-		<br /><em><?php p($l->t('WOPI Client')) ?></em>
-	</div>
+	<label for="wopi_url"><?php p($l->t('Collabora Online server')) ?></label>
+	<input type="text" name="wopi_url" id="wopi_url" value="<?php p($_['wopi_url'])?>" style="width:250px;">
+	<br /><em><?php p($l->t('URL (and port) of the Collabora Online server that provides the editing functionality as a WOPI client.')) ?></em>
 	<br /><button type="button" id="docs_apply"><?php p($l->t('Apply')) ?></button>
 	<span id="documents-admin-msg" class="msg"></span>
 </div>
