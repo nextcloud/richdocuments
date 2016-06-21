9a4b72711c2b95fa317ca57c82578b798c4d8a9f
Thomas Müller <thomas.mueller@tmit.eu>
2016-06-21 12:07:46 +0200

    remove old transifex code

--- l10n/hy.php
+++
@@ -1,7 +0,0 @@
-<?php
-$TRANSLATIONS = array(
-"Save" => "Պահպանել",
-"Close" => "Փակել",
-"Delete" => "Ջնջել"
-);
-$PLURAL_FORMS = "nplurals=2; plural=(n != 1);";
--- l10n/my_MM.php
+++
@@ -1,8 +0,0 @@
-<?php
-$TRANSLATIONS = array(
-"Cancel" => "ပယ်ဖျက်မည်",
-"OK" => "အိုကေ",
-"Text" => "စာသား",
-"Password" => "စကားဝှက်"
-);
-$PLURAL_FORMS = "nplurals=1; plural=0;";
