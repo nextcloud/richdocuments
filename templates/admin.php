99873b7331ce909d8c8ce438c8ba87b265b28491
Andras Timar <andras.timar@collabora.com>
2016-09-20 10:49:33 +0200

    change the id of Apply button to wopi_apply

--- templates/admin.php
+++ templates/admin.php
@@ -6,6 +6,6 @@ script('richdocuments', 'admin');
 	<label for="wopi_url"><?php p($l->t('Collabora Online server')) ?></label>
 	<input type="text" name="wopi_url" id="wopi_url" value="<?php p($_['wopi_url'])?>" style="width:300px;">
 	<br /><em><?php p($l->t('URL (and port) of the Collabora Online server that provides the editing functionality as a WOPI client.')) ?></em>
-	<br /><button type="button" id="docs_apply"><?php p($l->t('Apply')) ?></button>
+	<br /><button type="button" id="wopi_apply"><?php p($l->t('Apply')) ?></button>
 	<span id="documents-admin-msg" class="msg"></span>
 </div>
