4c1943b956f187708450d30f62df17d59b85397c
Pranav Kant <pranavk@collabora.co.uk>
2017-04-05 16:02:23 +0530

    Bin unused params
    These were passed earlier and used when creating a document from the
    dedicated richdocument's app page. But since that page is gone now and they
    are not used anymore, lets bin them.

--- lib/Controller/DocumentController.php
+++ lib/Controller/DocumentController.php
@@ -233,13 +233,8 @@ class DocumentController extends Controller {
 		if ($content && $view->file_put_contents($path, $content)) {
 			$info = $view->getFileInfo($path);
 			$ret = $this->wopiParser->getUrlSrc($mimetype);
-			$lolang = strtolower(str_replace('_', '-', $this->settings->getUserValue($this->uid, 'core', 'lang', 'en')));
 			$response =  array(
 				'status' => 'success',
-				'fileid' => $info['fileid'] . '_' . $this->settings->getSystemValue('instanceid'),
-				'urlsrc' => $ret['urlsrc'],
-				'action' => $ret['action'],
-				'lolang' => $lolang,
 				'data' => \OCA\Files\Helper::formatFileInfo($info)
 			);
 		} else {
