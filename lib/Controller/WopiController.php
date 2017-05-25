654c8e988d8296cf124b20b78ce2f992c18c60b9
Pranav Kant <pranavk@collabora.co.uk>
2017-05-25 19:05:33 +0530

    Show Display Name of user, not its uid in userlist
    (cherry picked from commit 3940ec64c7cf99ffaf70fcfc594fc49e5c2c415a)

--- lib/Controller/WopiController.php
+++ lib/Controller/WopiController.php
@@ -92,7 +92,7 @@ class WopiController extends Controller {
 				'Version' => $version,
 				'UserId' => $res['editor'] !== '' ? $res['editor'] : 'Guest user',
 				'OwnerId' => $res['owner'],
-				'UserFriendlyName' => $res['editor'] !== '' ? $res['editor'] : 'Guest user',
+				'UserFriendlyName' => $res['editor'] !== '' ? \OC_User::getDisplayName($res['editor']) : 'Guest user',
 				'UserCanWrite' => $res['canwrite'] ? true : false,
 				'PostMessageOrigin' => $res['server_host'],
 			]
