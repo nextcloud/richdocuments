7d502a31351a2ca2a4047297b74967b608e44193
Pranav Kant <pranavk@collabora.co.uk>
2017-06-02 00:16:30 +0530

    Revert "Replace 3 dots to ellisis in l10n files" partially
    This reverts commit c6a3e02b4b7af75341c573340ef11baef8bee39d partially.
    
    Presumably, some unwanted modifications also found its way in with above
    patch breaking the app. Revert problematic pieces.

--- templates/documents.php
+++ templates/documents.php
@@ -14,7 +14,7 @@ script('richdocuments', 'documents');
 ?>
 <div id="documents-content">
 	<ul class="documentslist">
-		<li class="progress icon-loading"><div><?php p($l->t('Loading documents…); ?></div></li>
+		<li class="progress icon-loading"><div><?php p($l->t('Loading documents…')); ?></div></li>
 		<li class="document template" data-id="" style="display:none;">
 			<a target="_blank" href=""><label></label></a>
 		</li>
