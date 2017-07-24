3eba8b76443a832f245839a1e744739bf087fa78
Pranav Kant <pranavk@collabora.co.uk>
2017-07-24 14:19:53 +0530

    Use fileId instead of dir
    (cherry picked from commit e9f9711421ed1eba9cdb3b787b1a49eaed7373d1)

--- lib/Controller/DocumentController.php
+++ lib/Controller/DocumentController.php
@@ -142,7 +142,7 @@ class DocumentController extends Controller {
 	 * @return TemplateResponse
 	 * @throws \Exception
 	 */
-	public function publicPage($shareToken, $fileName, $dir) {
+	public function publicPage($shareToken, $fileName, $fileId) {
 		try {
 			$share = $this->shareManager->getShareByToken($shareToken);
 			// not authenticated ?
@@ -156,7 +156,7 @@ class DocumentController extends Controller {
 
 			$node = $share->getNode();
 			if($node instanceof Folder) {
-				$item = $node->get($dir.'/'.$fileName);
+				$item = $node->getById($fileId)[0];
 			} else {
 				$item = $node;
 			}
