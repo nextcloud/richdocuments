c81dc834933a831f02081a2a5b9d07d5f37b8378
Roeland Jago Douma <roeland@famdouma.nl>
2018-07-10 12:28:29 +0200

    Cleanup config
    * Add appname constant to application
    * Remove unused functions
    
    Signed-off-by: Roeland Jago Douma <roeland@famdouma.nl>

--- lib/AppConfig.php
+++ lib/AppConfig.php
@@ -11,14 +11,15 @@
 
 namespace OCA\Richdocuments;
 
+use OCA\Richdocuments\AppInfo\Application;
 use \OCP\IConfig;
 
 class AppConfig{
-	private $appName = 'richdocuments';
 	private $defaults = [
 		'wopi_url' => 'https://localhost:9980'
 	];
 
+	/** @var IConfig */
 	private $config;
 
 	public function __construct(IConfig $config) {
@@ -35,41 +36,16 @@ class AppConfig{
 		if (array_key_exists($key, $this->defaults)){
 			$defaultValue = $this->defaults[$key];
 		}
-		return $this->config->getAppValue($this->appName, $key, $defaultValue);
+		return $this->config->getAppValue(Application::APPNAME, $key, $defaultValue);
 	}
 
 	/**
 	 * Set a value by key
 	 * @param string $key
 	 * @param string $value
-	 * @return string
+	 * @return void
 	 */
 	public function setAppValue($key, $value) {
-		return $this->config->setAppValue($this->appName, $key, $value);
-	}
-
-	/**
-	 * Get a value by key for a user
-	 * @param string $userId
-	 * @param string $key
-	 * @return string
-	 */
-	public function getUserValue($userId, $key) {
-		$defaultValue = null;
-		if (array_key_exists($key, $this->defaults)){
-			$defaultValue = $this->defaults[$key];
-		}
-		return $this->config->getUserValue($userId, $this->appName, $key, $defaultValue);
-	}
-
-	/**
-	 * Set a value by key for a user
-	 * @param string $userId
-	 * @param string $key
-	 * @param string $value
-	 * @return string
-	 */
-	public function setUserValue($userId, $key, $value) {
-		return $this->config->setAppValue($userId, $this->appName, $key, $value);
+		$this->config->setAppValue(Application::APPNAME, $key, $value);
 	}
  }
