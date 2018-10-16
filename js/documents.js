4830f6c2390091b0235eb27944519c80ad08c7dc
Roeland Jago Douma <roeland@famdouma.nl>
2018-10-16 23:14:38 +0200

    Do not show loading screen if we need a nickname
    Fixes #280
    
    If we require the user to enter a password. We should not show the
    loading screen. As this overlays the input field as well.
    
    Signed-off-by: Roeland Jago Douma <roeland@famdouma.nl>

--- js/documents.js
+++ js/documents.js
@@ -613,7 +613,7 @@ var documentsMain = {
 
 		documentsMain.show(fileId);
 
-		if (fileId) {
+		if (fileId && Number.isInteger(Number(fileId))) {
 			documentsMain.overlay.documentOverlay('show');
 			documentsMain.prepareSession();
 		}
