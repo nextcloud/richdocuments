129516e4f5f8576765faf000b461398075153c87
Roeland Jago Douma <roeland@famdouma.nl>
2018-11-08 13:41:49 +0100

    Add Personal settings
    Signed-off-by: Roeland Jago Douma <roeland@famdouma.nl>

---
+++ lib/Settings/Personal.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * @copyright Copyright (c) 2018, Roeland Jago Douma <roeland@famdouma.nl>
+ *
+ * @author Roeland Jago Douma <roeland@famdouma.nl>
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+namespace OCA\Richdocuments\Settings;
+
+use OCA\Richdocuments\TemplateManager;
+use OCP\AppFramework\Http\TemplateResponse;
+use OCP\IConfig;
+use OCP\Settings\ISettings;
+
+class Personal implements ISettings {
+
+	/**
+	 * @return TemplateResponse
+	 */
+	public function getForm() {
+		return new TemplateResponse(
+			'richdocuments',
+			'personal',
+			[],
+			'blank'
+		);
+	}
+	/**
+	 * @return string the section ID, e.g. 'sharing'
+	 */
+	public function getSection() {
+		return 'richdocuments';
+	}
+	/**
+	 * @return int whether the form should be rather on the top or bottom of
+	 * the admin section. The forms are arranged in ascending order of the
+	 * priority values. It is required to return a value between 0 and 100.
+	 *
+	 * keep the server setting at the top, right after "server settings"
+	 */
+	public function getPriority() {
+		return 0;
+	}
+}
---
+++ templates/personal.php
@@ -0,0 +1,5 @@
+<div class="section" id="richdocuments">
+	<h2>
+		<?php p($l->t('Collabora Online')) ?>
+	</h2>
+</div>
