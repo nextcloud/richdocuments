6f8798e0be3967a016f920f5b7e690b576467985
Julius Härtl <jus@bitgrid.net>
2019-01-24 16:15:54 +0100

    Use templates shipped with richdocuments if template selection isn't available
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

--- appinfo/app.php
+++ appinfo/app.php
@@ -58,6 +58,10 @@ if (class_exists('\OC\Files\Type\TemplateManager')) {
 	$manager->registerTemplate('application/vnd.openxmlformats-officedocument.wordprocessingml.document', dirname(__DIR__) . '/assets/docxtemplate.docx');
 	$manager->registerTemplate('application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', dirname(__DIR__) . '/assets/xlsxtemplate.xlsx');
 	$manager->registerTemplate('application/vnd.openxmlformats-officedocument.presentationml.presentation', dirname(__DIR__) . '/assets/pptxtemplate.pptx');
+	$manager->registerTemplate('application/vnd.oasis.opendocument.presentation', dirname(__DIR__) . '/assets/presentation.otp');
+	$manager->registerTemplate('application/vnd.oasis.opendocument.text', dirname(__DIR__) . '/assets/document.ott');
+	$manager->registerTemplate('application/vnd.oasis.opendocument.spreadsheet', dirname(__DIR__) . '/assets/spreadsheet.ots');
+
 }
 
 // Whitelist the wopi URL for iframes, required for Firefox
