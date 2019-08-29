a21b33b1013b122efcec619da4a41f885d8a4ac1
Julius Härtl <jus@bitgrid.net>
2019-08-29 09:44:42 +0200

    Cleanup public wopi URL since otherwise it doesn't work in the CSP headers
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

--- lib/Controller/SettingsController.php
+++ lib/Controller/SettingsController.php
@@ -158,8 +158,7 @@ class SettingsController extends Controller{
 		try {
 			$capaUrlSrc = $this->wopiParser->getUrlSrc('Capabilities');
 			if (is_array($capaUrlSrc) && $capaUrlSrc['action'] === 'getinfo') {
-				// XXX Works, but probably better to strip trailing '/hosting/capabilities'
-				$public_wopi_url = $capaUrlSrc['urlsrc'];
+				$public_wopi_url = str_replace('/hosting/capabilities', '', $capaUrlSrc['urlsrc']);
 				if ($public_wopi_url !== null) {
 					$this->appConfig->setAppValue('public_wopi_url', $public_wopi_url);
 					$colon = strpos($public_wopi_url, ':', 0);
