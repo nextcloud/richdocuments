0090f8369f0986bcacb6dcb0eb45ffdef562f6a3
KERIOU Samir <samir.keriou@gendarmerie.interieur.gouv.fr>
2020-06-24 11:41:10 +0200

    Fix bug #1054
    Signed-off-by: KERIOU Samir <samir.keriou@gendarmerie.interieur.gouv.fr>
    
    reindent code, new migration
    
    Signed-off-by: KERIOU Samir <samir.keriou@gendarmerie.interieur.gouv.fr>
    
    fix indent, new migration
    
    Signed-off-by: KERIOU Samir <samir.keriou@gendarmerie.interieur.gouv.fr>

---
+++ lib/Migration/Version030702Date20200626072306.php
@@ -0,0 +1,97 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OCA\Richdocuments\Migration;
+
+use Closure;
+use OCP\DB\ISchemaWrapper;
+use OCP\Migration\SimpleMigrationStep;
+use OCP\Migration\IOutput;
+
+/**
+ * Auto-generated migration step: Please modify to your needs!
+ */
+class Version030702Date20200626072306 extends SimpleMigrationStep {
+
+	/**
+	 * @param IOutput $output
+	 * @param Closure $schemaClosure The `\Closure` returns a `ISchemaWrapper`
+	 * @param array $options
+	 */
+	public function preSchemaChange(IOutput $output, Closure $schemaClosure, array $options) {
+	}
+
+	/**
+	 * @param IOutput $output
+	 * @param Closure $schemaClosure The `\Closure` returns a `ISchemaWrapper`
+	 * @param array $options
+	 * @return null|ISchemaWrapper
+	 */
+	public function changeSchema(IOutput $output, Closure $schemaClosure, array $options) {
+		/** @var ISchemaWrapper $schema */
+		$schema = $schemaClosure();
+
+		$table = $schema->getTable('richdocuments_wopi');
+
+		if (!$table->hasColumn('template_id')) {
+			$table->addColumn('template_id', 'integer', [
+				'notnull' => false,
+				'length' => 4,
+			]);
+		}
+
+		if (!$table->hasColumn('hide_download')) {
+			$table->addColumn('hide_download', 'boolean', [
+				'notnull' => true,
+				'default' => false,
+			]);
+		}
+
+		if (!$table->hasColumn('share')) {
+			$table->addColumn('share', 'string', [
+				'notnull' => false,
+				'length' => 64
+			]);
+		}
+
+		if (!$table->hasColumn('direct')) {
+			$table->addColumn('direct', 'boolean', [
+				'notnull' => true,
+				'default' => false,
+			]);
+		}
+		if (!$table->hasColumn('is_remote_token')) {
+			$table->addColumn('is_remote_token', 'boolean', [
+				'notnull' => true,
+				'default' => false,
+			]);
+		}
+
+		if (!$table->hasColumn('remote_server')) {
+			$table->addColumn('remote_server', 'string', [
+				'notnull' => true,
+				'default' => '',
+			]);
+
+		}
+		if (!$table->hasColumn('remote_server_token')) {
+			$table->addColumn('remote_server_token', 'string', [
+				'notnull' => true,
+				'length' => 32,
+				'default' => '',
+			]);
+		}
+
+
+		return $schema;
+	}
+
+	/**
+	 * @param IOutput $output
+	 * @param Closure $schemaClosure The `\Closure` returns a `ISchemaWrapper`
+	 * @param array $options
+	 */
+	public function postSchemaChange(IOutput $output, Closure $schemaClosure, array $options) {
+	}
+}
