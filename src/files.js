267f3d813a66dbda012227e7416314399e87388f
Szymon Kłos <szymon.klos@collabora.com>
2021-04-02 12:51:12 +0200

    Reload file name for share feature after Save As
    There was a bug that after save as the sidebar was showing
    the old file when we used share feature.
    
    Signed-off-by: Szymon Kłos <szymon.klos@collabora.com>

--- src/files.js
+++ src/files.js
@@ -368,7 +368,7 @@ $(document).ready(function() {
 			FilesAppIntegration.rename(args.NewName)
 			break
 		case 'Action_Save_Resp':
-			FilesAppIntegration.saveAs()
+			FilesAppIntegration.saveAs(args.fileName)
 			break
 		case 'close':
 			odfViewer.onClose()
