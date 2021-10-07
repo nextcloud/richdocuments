32b986af3ff5679fde25221f2cb42dae8255eaaf
Julius Härtl <jus@bitgrid.net>
2021-10-07 08:50:51 +0200

    Unify ui_defaults logic and hide statusbar
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

---
+++ src/helpers/coolParameters.js
@@ -0,0 +1,79 @@
+/*
+ * @copyright Copyright (c) 2021 Julius Härtl <jus@bitgrid.net>
+ *
+ * @author Julius Härtl <jus@bitgrid.net>
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+const getUIDefaults = () => {
+	const statusBar = 'false'
+	const textRuler = 'false'
+	const sidebar = 'false'
+	const uiMode = 'classic' // or notebookbar
+
+	let uiDefaults = 'TextRuler=' + textRuler + ';'
+	uiDefaults += 'TextSidebar=' + sidebar + ';TextStatusbar=' + statusBar + ';'
+	uiDefaults += 'PresentationSidebar=' + sidebar + ';PresentationStatusbar=' + statusBar + ';'
+	uiDefaults += 'SpreadsheetSidebar=' + sidebar + ';SpreadsheetStatusbar=' + statusBar + ';'
+	uiDefaults += 'UIMode=' + uiMode + ';'
+	return uiDefaults
+}
+
+const generateCSSVarTokens = () => {
+	/* NC versus COOL */
+	const cssVarMap = {
+		'--color-primary-text': '--co-primary-text',
+		'--color-primary-element': '--co-primary-element:--co-text-accent',
+		'--color-primary-element-light': '--co-primary-element-light',
+		'--color-error': '--co-color-error',
+		'--color-warning': '--co-color-warning',
+		'--color-success': '--co-color-success',
+		'--border-radius': '--co-border-radius',
+		'--border-radius-large': '--co-border-radius-large',
+		'--color-loading-light': '--co-loading-light',
+		'--color-loading-dark': '--co-loading-dark',
+		'--color-box-shadow': '--co-box-shadow',
+		'--color-border': '--co-border',
+		'--color-border-dark': '--co-border-dark',
+		'--border-radius-pill': '--co-border-radius-pill',
+		'--font-face': '--loleaflet-font',
+	}
+	let str = ''
+	try {
+		for (const cssVarKey in cssVarMap) {
+			let cStyle = window.parent.getComputedStyle(document.documentElement).getPropertyValue(cssVarKey)
+			if (!cStyle) {
+				// try suffix -dark instead
+				cStyle = window.parent.getComputedStyle(document.documentElement).getPropertyValue(cssVarKey + '-dark')
+			}
+			if (!cStyle) continue // skip if it is not set
+			const varNames = cssVarMap[cssVarKey].split(':')
+			for (let i = 0; i < varNames.length; ++i) {
+				str += varNames[i] + '=' + cStyle + ';'
+			}
+		}
+	} catch (e) {
+		// Skip extracting css vars if we cannot access parent
+	}
+	return str
+}
+
+export {
+	getUIDefaults,
+	generateCSSVarTokens,
+}
