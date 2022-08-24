f27153f7fb43cc57fc8b9dd9f0c812ee5246c0dc
Raul <r.ferreira.fuentes@gmail.com>
2022-08-24 12:00:12 +0200

    Hide "Save as" option when downloading is not allowed by share
    Signed-off-by: Raul <r.ferreira.fuentes@gmail.com>

--- lib/Controller/WopiController.php
+++ lib/Controller/WopiController.php
@@ -200,7 +200,7 @@ class WopiController extends Controller {
 			'UserFriendlyName' => $userDisplayName,
 			'UserExtraInfo' => [],
 			'UserCanWrite' => (bool)$wopi->getCanwrite(),
-			'UserCanNotWriteRelative' => $this->encryptionManager->isEnabled() || $isPublic,
+			'UserCanNotWriteRelative' => $isPublic || $this->encryptionManager->isEnabled() || $wopi->getHideDownload(),
 			'PostMessageOrigin' => $wopi->getServerHost(),
 			'LastModifiedTime' => Helper::toISO8601($file->getMTime()),
 			'SupportsRename' => !$isVersion,
