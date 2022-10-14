3428c1f944fa228818309e3c38431268ad92839f
Julius Härtl <jus@bitgrid.net>
2022-10-14 10:08:49 +0200

    Disable sharing on public links
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

--- lib/Controller/WopiController.php
+++ lib/Controller/WopiController.php
@@ -206,7 +206,7 @@ class WopiController extends Controller {
 			'SupportsRename' => !$isVersion,
 			'UserCanRename' => !$isPublic && !$isVersion,
 			'EnableInsertRemoteImage' => !$isPublic,
-			'EnableShare' => $file->isShareable() && !$isVersion,
+			'EnableShare' => $file->isShareable() && !$isVersion && !$isPublic,
 			'HideUserList' => '',
 			'DisablePrint' => $wopi->getHideDownload(),
 			'DisableExport' => $wopi->getHideDownload(),
