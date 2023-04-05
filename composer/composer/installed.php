3655d1559827c7ec2f32e8ea9b5b1be6173f4d54
Julius Härtl <jus@bitgrid.net>
2023-04-05 20:14:04 +0200

    chore: Bump composer autoloader for new composer version
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

--- composer/composer/installed.php
+++ composer/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'adf99cde6d5ce9132f1f954aebcb8e7bc19e41d9',
+        'reference' => 'efaaf7b1fb320e74f33d9f8f5b58b1d358805689',
         'type' => 'library',
         'install_path' => __DIR__ . '/../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'adf99cde6d5ce9132f1f954aebcb8e7bc19e41d9',
+            'reference' => 'efaaf7b1fb320e74f33d9f8f5b58b1d358805689',
             'type' => 'library',
             'install_path' => __DIR__ . '/../',
             'aliases' => array(),
