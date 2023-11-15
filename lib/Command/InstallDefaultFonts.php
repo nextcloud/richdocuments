e1719374a897d4bb21cebcbd31b8b596a8b37cdd
Luka Trovic <luka@nextcloud.com>
2023-11-15 20:38:02 +0100

    feat: add repair step to install default fonts on installation
    Signed-off-by: Luka Trovic <luka@nextcloud.com>

---
+++ lib/Command/InstallDefaultFonts.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace OCA\Richdocuments\Command;
+
+use OCA\Richdocuments\Service\FontService;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class InstallDefaultFonts extends Command {
+	public function __construct(private FontService $fontService) {
+		parent::__construct();
+	}
+
+	protected function configure() {
+		$this
+			->setName('richdocuments:install-fonts')
+			->setDescription('Install default fonts');
+	}
+
+	protected function execute(InputInterface $input, OutputInterface $output) {
+		try {
+			$this->fontService->installDefaultFonts();
+			return 0;
+		} catch (\Exception $e) {
+			$output->writeln('<error>Failed to install default fonts</error>');
+			$output->writeln($e->getMessage());
+			$output->writeln($e->getTraceAsString());
+			return 1;
+		}
+	}
+}
