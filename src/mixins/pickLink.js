0e8a6f16c02c0dc65008bf925593be331d849824
Julius Härtl <jus@bitgrid.net>
2023-11-23 16:08:23 +0100

    fixup! chore(settings): Switch to NcSelect

--- src/mixins/pickLink.js
+++ src/mixins/pickLink.js
@@ -21,7 +21,7 @@
 
 import { generateOcsUrl } from '@nextcloud/router'
 import axios from '@nextcloud/axios'
-import { getLinkWithPicker } from '@nextcloud/vue'
+import { getLinkWithPicker } from '@nextcloud/vue/dist/Components/NcRichText.js'
 
 // FIXME: Migrate to vue component
 export default {
