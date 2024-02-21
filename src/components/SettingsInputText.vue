1643d254ab290a94bec85fee011bd0e81d8ed2a6
Julius Härtl <jus@bitgrid.net>
2024-02-21 12:51:13 +0100

    fix: Properly use input model in settings text fields
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

--- src/components/SettingsInputText.vue
+++ src/components/SettingsInputText.vue
@@ -21,18 +21,17 @@
   -->
 
 <template>
-	<form @submit.prevent="">
+	<form @submit.prevent="submit">
 		<div class="input-wrapper">
 			<label :for="id">{{ label }}</label>
 			<input :id="id"
+				v-model="inputVal"
 				type="text"
-				:value="inputVal"
 				:disabled="disabled"
 				@input="$emit('input', $event.target.value)">
 			<input type="submit"
 				class="icon-confirm"
-				value=""
-				@click="$emit('update', inputVal)">
+				value="">
 		</div>
 		<p v-if="hint !== ''" class="hint">
 			{{ hint }}
@@ -74,12 +73,17 @@ export default {
 	},
 	watch: {
 		value(newVal) {
-			this.inputVal = this.value
+			this.inputVal = newVal
 		},
 	},
 	beforeCreate() {
 		this.uuid = uuid.toString()
 		uuid += 1
+	},
+	methods: {
+		submit() {
+			this.$emit('update', this.inputVal)
+		},
 	},
 }
 </script>
