c70145ddd609205512d56c4a7c37c49ef81cf7b1
Julius Härtl <jus@bitgrid.net>
2024-03-26 16:54:52 +0100

    fix: Adjust cypress selector for file version
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

--- cypress/e2e/integration.spec.js
+++ cypress/e2e/integration.spec.js
@@ -73,7 +73,7 @@ describe('Nextcloud integration', function() {
 
 		cy.get('#tab-version_vue').should('be.visible')
 
-		cy.get('#tab-version_vue .line-one__name').contains('Current version')
+		cy.get('#tab-version_vue .version__info__label').contains('Current version')
 	})
 
 	it('Save as', function() {
