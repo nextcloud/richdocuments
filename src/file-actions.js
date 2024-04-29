43a82631d2ccc611e07c4e224dde1c45db73638c
Elizabeth Danzberger <lizzy7128@tutanota.de>
2024-04-29 19:07:30 -0400

    fix: file action for opening pdf files with richdocuments
    Signed-off-by: Elizabeth Danzberger <lizzy7128@tutanota.de>

---
+++ src/file-actions.js
@@ -0,0 +1,40 @@
+import { registerFileAction, FileAction } from '@nextcloud/files'
+import { getCapabilities } from '@nextcloud/capabilities'
+import { translate as t } from '@nextcloud/l10n'
+
+// eslint-disable-next-line import/no-unresolved
+import appIcon from '../img/app.svg?raw'
+
+const richdocuments = getCapabilities().richdocuments
+
+const openPdf = new FileAction({
+	id: 'office-open-pdf',
+
+	iconSvgInline: () => {
+		// Make sure the icon is the correct color
+		return appIcon.replaceAll(/#(fff|0{6})/g, 'currentColor')
+	},
+
+	displayName: () => {
+		return t('richdocuments',
+			'Edit with {productName}',
+			{ productName: richdocuments.productName })
+	},
+
+	enabled: () => {
+		// Only enable the file action when files_pdfviewer is enabled
+		const optionalMimetypes = richdocuments.mimetypesNoDefaultOpen
+		return optionalMimetypes.includes('application/pdf')
+	},
+
+	exec: (file) => {
+		// If no viewer API, we can't open the document
+		if (!OCA.Viewer) {
+			return
+		}
+
+		OCA.Viewer.openWith('richdocuments', { path: file.path })
+	},
+})
+
+registerFileAction(openPdf)
