96e98e85dbd261935cb02792b4e16c436b59b57a
Andy Scherzinger <info@andy-scherzinger.de>
2024-05-09 10:14:17 +0200

    fix: Add further SPDX headers
    Signed-off-by: Andy Scherzinger <info@andy-scherzinger.de>

--- webpack.js
+++ webpack.js
@@ -1,3 +1,7 @@
+/**
+ * SPDX-FileCopyrightText: 2021 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
 const path = require('path')
 const webpackConfig = require('@nextcloud/webpack-vue-config')
 const webpackRules = require('@nextcloud/webpack-vue-config/rules')
