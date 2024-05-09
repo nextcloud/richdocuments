13edfd3fe8c5eedfc72c1cb4f42981fdfe88f278
Andy Scherzinger <info@andy-scherzinger.de>
2024-05-09 16:07:22 +0200

    fix: php lint
    Signed-off-by: Andy Scherzinger <info@andy-scherzinger.de>

--- tests/features/bootstrap/DirectContext.php
+++ tests/features/bootstrap/DirectContext.php
@@ -1,10 +1,10 @@
 <?php
+
 declare(strict_types=1);
 /**
  * SPDX-FileCopyrightText: 2021 Nextcloud GmbH and Nextcloud contributors
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
-
 use Behat\Behat\Context\Context;
 use Behat\Behat\Hook\Scope\BeforeScenarioScope;
 use GuzzleHttp\Client;
