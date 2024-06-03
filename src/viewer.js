95a7d531253be43f16ce13625e0801ee5e171af8
Elizabeth Danzberger <lizzy7128@tutanota.de>
2024-06-03 14:27:11 -0400

    fix: remove legacy viewer code
    Signed-off-by: Elizabeth Danzberger <lizzy7128@tutanota.de>

--- src/viewer.js
+++ src/viewer.js
@@ -39,29 +39,3 @@ if (OCA.Viewer) {
 		canCompare: true,
 	})
 }
-
-// TODO: Viewer.openWith introduced with https://github.com/nextcloud/viewer/pull/1273
-//       This check can be replaced with `if(OCA.Viewer)` once NC 24 is EOL.
-if (OCA.Viewer.openWith && OCA?.Files?.fileActions) {
-	const supportedMimes = getCapabilities().richdocuments.mimetypesNoDefaultOpen
-	const actionName = 'Edit with ' + getCapabilities().richdocuments.productName
-	const actionDisplayNameEdit = t('richdocuments', 'Edit with {productName}', { productName: getCapabilities().richdocuments.productName }, undefined, { escape: false })
-	const actionDisplayNameOpen = t('richdocuments', 'Open with {productName}', { productName: getCapabilities().richdocuments.productName }, undefined, { escape: false })
-
-	for (const mime of supportedMimes) {
-		const action = {
-			name: actionName,
-			mime,
-			permissions: OC.PERMISSION_READ,
-			iconClass: 'icon-richdocuments',
-			displayName: mime === 'application/pdf' ? actionDisplayNameOpen : actionDisplayNameEdit,
-			actionHandler: (fileName, context) => {
-				OCA.Viewer.openWith('richdocuments', {
-					path: context.fileInfoModel.getFullPath(),
-				})
-			},
-		}
-
-		OCA.Files.fileActions.registerAction(action)
-	}
-}
