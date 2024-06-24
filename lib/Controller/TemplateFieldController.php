7a83486cef62e4a1bef2d7579a55eeb9c56cad28
Elizabeth Danzberger <lizzy7128@tutanota.de>
2024-06-24 11:38:07 -0400

    fix: Add `fileId` param to route path
    Signed-off-by: Elizabeth Danzberger <lizzy7128@tutanota.de>

--- lib/Controller/TemplateFieldController.php
+++ lib/Controller/TemplateFieldController.php
@@ -2,24 +2,45 @@
 
 namespace OCA\Richdocuments\Controller;
 
+use OCA\Richdocuments\Service\TemplateFieldService;
+use OCA\Richdocuments\TemplateManager;
 use OCP\AppFramework\Http;
-use OCP\AppFramework\OCSController;
 use OCP\AppFramework\Http\DataResponse;
+use OCP\AppFramework\OCSController;
 use OCP\Files\Node;
 use OCP\IRequest;
 
+// TODO: Probably move this into TemplatesController.php at some point
 class TemplateFieldController extends OCSController {
+
+	private TemplateFieldService $templateFieldService;
+	private TemplateManager $templateManager;
+
 	public function __construct(
 		string $appName,
-		IRequest $request
+		IRequest $request,
+		TemplateFieldService $templateFieldService,
+		TemplateManager $templateManager
 	) {
 		parent::__construct($appName, $request);
+
+		$this->templateFieldService = $templateFieldService;
+		$this->templateManager = $templateManager;
 	}
 
-    public function extractFields(): DataResponse {
-        $this->buildResponse()
-        return new DataResponse(['something' => 'hello!'], Http::STATUS_OK);
-    }
+	/**
+	 * @param int $fileId
+	 * @return DataResponse
+	 */
+	public function extractFields(int $fileId): DataResponse {
+		try {
+			$template = $this->templateManager->get($fileId);
+			return new DataResponse(['something' => $template], Http::STATUS_OK);
+		} catch (NotFoundException $e) {
+			return new DataResponse([], Http::STATUS_NOT_FOUND);
+		}
+	}
 
-    public function fillFields(Node $file, array $fieldValues): void {}
+	public function fillFields(Node $file, array $fieldValues): void {
+	}
 }
--- lib/Service/TemplateFieldService.php
+++ lib/Service/TemplateFieldService.php
@@ -2,7 +2,17 @@
 
 namespace OCA\Richdocuments\Service;
 
+use OCP\Files\Node;
+
 class TemplateFieldService {
 	public function __construct() {
+	}
+
+	public function extractFields(Node $file): ?array {
+		return null;
+	}
+
+	public function fillFields(Node $file, array $fieldValues) {
+		
 	}
 }
