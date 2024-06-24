9cdef534290a7a80a68c41c6d9d96817224b61c3
Elizabeth Danzberger <lizzy7128@tutanota.de>
2024-06-24 10:50:21 -0400

    testing
    Signed-off-by: Elizabeth Danzberger <lizzy7128@tutanota.de>

--- lib/Controller/TemplateFieldController.php
+++ lib/Controller/TemplateFieldController.php
@@ -2,7 +2,10 @@
 
 namespace OCA\Richdocuments\Controller;
 
+use OCP\AppFramework\Http;
 use OCP\AppFramework\OCSController;
+use OCP\AppFramework\Http\DataResponse;
+use OCP\Files\Node;
 use OCP\IRequest;
 
 class TemplateFieldController extends OCSController {
@@ -12,4 +15,11 @@ class TemplateFieldController extends OCSController {
 	) {
 		parent::__construct($appName, $request);
 	}
+
+    public function extractFields(): DataResponse {
+        $this->buildResponse()
+        return new DataResponse(['something' => 'hello!'], Http::STATUS_OK);
+    }
+
+    public function fillFields(Node $file, array $fieldValues): void {}
 }
