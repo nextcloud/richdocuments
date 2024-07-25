ab1b4929235ea2f027affd4b8809153d622b837c
Julius Härtl <jus@bitgrid.net>
2024-07-25 09:23:11 +0200

    fix: Do not ask for guest name on read only pdfs
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

--- src/view/Office.vue
+++ src/view/Office.vue
@@ -175,6 +175,14 @@ export default {
 			type: String,
 			default: null,
 		},
+		mime: {
+			type: String,
+			default: null,
+		},
+		permissions: {
+			type: String,
+			default: '',
+		},
 		isEmbedded: {
 			type: Boolean,
 			default: false,
@@ -284,7 +292,7 @@ export default {
 		}
 		this.postMessage.registerPostMessageHandler(this.postMessageHandler)
 
-		if (shouldAskForGuestName()) {
+		if (shouldAskForGuestName(this.mime, this.permissions?.includes('W'))) {
 			const { default: GuestNamePicker } = await import(
 				/* webpackChunkName: 'GuestNamePicker' */
 				'../components/GuestNamePicker.vue')
