291d70442338cac107c0928f42145714bfd4b218
Elizabeth Danzberger <lizzy7128@tutanota.de>
2024-08-05 13:16:29 -0400

    Fix new file menu on public shares
    Signed-off-by: Elizabeth Danzberger <lizzy7128@tutanota.de>

--- src/public.js
+++ src/public.js
@@ -3,21 +3,17 @@ import './init-shared.js'
 import isPublic from './helpers/isPublicPage.js'
 import isDocument from './helpers/isDocument.js'
 import isPdf from './helpers/isPdf.js'
+import NewFileMenu from './view/NewFileMenu.js'
 
 document.addEventListener('DOMContentLoaded', () => {
-	const validMimetype = (isDocument() || isPdf())
-
-	// Viewer app not enabled
-	if (!OCA.Viewer) {
+	if (!isPublic() || !OCA.Viewer) {
 		return
 	}
 
-	// Not a public share or is not a valid mimetype
-	if (!isPublic() || !validMimetype) {
-		return
+	if (OCA.Files && OCA.Files.fileActions) {
+		OC.Plugins.register('OCA.Files.NewFileMenu', NewFileMenu)
 	}
 
-	// If the share is a PDF or another supported document, we proceed to open it
 	if (isPdf()) {
 		OCA.Viewer.openWith('richdocuments', { path: '/' })
 	} else if (isDocument()) {
