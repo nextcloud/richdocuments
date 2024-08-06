51d641a254c4a794a350297c544a9834630f9555
Elizabeth Danzberger <lizzy7128@tutanota.de>
2024-08-06 17:36:59 -0400

    fix(share): Open with richdocuments if PDF and download is hidden
    Signed-off-by: Elizabeth Danzberger <lizzy7128@tutanota.de>

--- src/public.js
+++ src/public.js
@@ -2,9 +2,12 @@
  * SPDX-FileCopyrightText: 2024 Nextcloud GmbH and Nextcloud contributors
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
-import isPublic from './helpers/isPublicPage.js'
-import isDocument from './helpers/isDocument.js'
-import isPdf from './helpers/isPdf.js'
+
+import {
+	isPublic,
+	isPdf,
+	isDownloadHidden,
+} from './helpers/index.js'
 import NewFileMenu from './view/NewFileMenu.js'
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -16,9 +19,9 @@ document.addEventListener('DOMContentLoaded', () => {
 		OC.Plugins.register('OCA.Files.NewFileMenu', NewFileMenu)
 	}
 
-	if (isPdf()) {
+	if (isDownloadHidden() && isPdf()) {
 		OCA.Viewer.openWith('richdocuments', { path: '/' })
-	} else if (isDocument()) {
+	} else {
 		OCA.Viewer.open({ path: '/' })
 	}
 })
