b9bd8705e58bf86c7dcca8b9989495e6d97e4c35
Julius Härtl <jus@bitgrid.net>
2024-08-20 12:52:49 +0200

    Revert "ci: Fix selector for saveas button"
    This reverts commit f790b57e565d646a7b658515191bfb7b2f2df4f6.
    
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

--- cypress/e2e/integration.spec.js
+++ cypress/e2e/integration.spec.js
@@ -76,8 +76,8 @@ describe('Nextcloud integration', function() {
 		cy.get('#tab-version_vue .list-item-content__name').contains('Current version')
 	})
 
-	// Currently it seems that Collabora is missing the save as button
 	it('Save as', function() {
+		const exportFilename = 'document.rtf'
 		cy.get('@loleafletframe').within(() => {
 			cy.get('#File-tab-label').click()
 			cy.get('#saveas').click()
@@ -87,7 +87,7 @@ describe('Nextcloud integration', function() {
 		cy.get('.oc-dialog').should('be.visible')
 		cy.get('.oc-dialog input[type=text]')
 			.should('be.visible')
-			.should('have.value', 'document.rtf')
+			.should('have.value', `/${exportFilename}`)
 
 		cy.get('.oc-dialog button.primary').click()
 
@@ -99,7 +99,7 @@ describe('Nextcloud integration', function() {
 		// FIXME: We should not need to reload
 		cy.get('.breadcrumb__crumbs a').eq(0).click()
 
-		cy.openFile('document.rtf')
+		cy.openFile(exportFilename)
 	})
 
 	it('Open locally', function() {
