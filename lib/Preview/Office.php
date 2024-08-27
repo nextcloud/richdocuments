bea1958f3b6a17ce08a8b1b2962ae8138bcc3b87
Julius Härtl <jus@bitgrid.net>
2024-08-27 18:21:47 +0200

    feat: Add parameter to allow converting files after filling out fields
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

--- lib/Preview/Office.php
+++ lib/Preview/Office.php
@@ -5,12 +5,10 @@
  */
 namespace OCA\Richdocuments\Preview;
 
-use OCA\Richdocuments\AppConfig;
 use OCA\Richdocuments\Capabilities;
-use OCA\Richdocuments\Service\RemoteOptionsService;
+use OCA\Richdocuments\Service\RemoteService;
 use OCP\Files\File;
 use OCP\Files\FileInfo;
-use OCP\Http\Client\IClientService;
 use OCP\IImage;
 use OCP\Image;
 use OCP\Preview\IProviderV2;
@@ -20,10 +18,9 @@ abstract class Office implements IProviderV2 {
 	private array $capabilities;
 
 	public function __construct(
-		private IClientService $clientService,
-		private AppConfig $config,
-		Capabilities $capabilities,
+		private RemoteService $remoteService,
 		private LoggerInterface $logger,
+		Capabilities $capabilities,
 	) {
 		$this->capabilities = $capabilities->getCapabilities()['richdocuments'] ?? [];
 	}
@@ -35,47 +32,25 @@ abstract class Office implements IProviderV2 {
 		return false;
 	}
 
-	/**
-	 * {@inheritDoc}
-	 */
 	public function getThumbnail(File $file, int $maxX, int $maxY): ?IImage {
 		if ($file->getSize() === 0) {
 			return null;
 		}
 
-		$useTempFile = $file->isEncrypted() || !$file->getStorage()->isLocal();
-		if ($useTempFile) {
-			$fileName = $file->getStorage()->getLocalFile($file->getInternalPath());
-			$stream = fopen($fileName, 'r');
-		} else {
-			$stream = $file->fopen('r');
-		}
-
-		$client = $this->clientService->newClient();
-		$options = RemoteOptionsService::getDefaultOptions();
-		// FIXME: can be removed once https://github.com/CollaboraOnline/online/issues/6983 is fixed upstream
-		$options['expect'] = false;
-
-		if ($this->config->getAppValue('richdocuments', 'disable_certificate_verification') === 'yes') {
-			$options['verify'] = false;
-		}
-
-		$options['multipart'] = [['name' => $file->getName(), 'contents' => $stream]];
-
 		try {
-			$response = $client->post($this->config->getCollaboraUrlInternal() . '/cool/convert-to/png', $options);
+			$response = $this->remoteService->convertFileTo($file, 'png');
+			$image = new Image();
+			$image->loadFromData($response);
+
+			if ($image->valid()) {
+				$image->scaleDownToFit($maxX, $maxY);
+				return $image;
+			}
 		} catch (\Exception $e) {
 			$this->logger->info('Failed to convert preview: ' . $e->getMessage(), ['exception' => $e]);
 			return null;
 		}
 
-		$image = new Image();
-		$image->loadFromData($response->getBody());
-
-		if ($image->valid()) {
-			$image->scaleDownToFit($maxX, $maxY);
-			return $image;
-		}
 		return null;
 	}
 }
