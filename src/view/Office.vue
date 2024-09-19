3790afca983e8701f22d6b38f1b4077e608c1142
Julius Knorr <jus@bitgrid.net>
2024-09-19 20:40:20 +0200

    fix: Properly render compare view
    Signed-off-by: Julius Knorr <jus@bitgrid.net>

--- src/view/Office.vue
+++ src/view/Office.vue
@@ -532,11 +532,20 @@ export default {
 </style>
 
 <style lang="scss">
-.viewer .office-viewer:not(.viewer__file--hidden) {
+.viewer__content:not(.viewer--split) .office-viewer:not(.viewer__file--hidden):not(.widget-file) {
 	width: 100%;
 	height: 100vh;
 	height: 100dvh;
 	top: -50px;
 	position: absolute;
+}
+
+[data-handler='richdocuments'] .modal-container {
+	bottom: 0;
+}
+
+.viewer__content.viewer--split .office-viewer {
+	height: 100%;
+	width: 100%;
 }
 </style>
