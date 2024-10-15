d0a91043c3edb2d4a2ea1cf5098547a07809dc28
Elizabeth Danzberger <lizzy7128@tutanota.de>
2024-10-14 20:52:08 -0400

    fix(spdx): add spdx header to global templates component
    Signed-off-by: Elizabeth Danzberger <lizzy7128@tutanota.de>

--- src/components/AdminSettings/GlobalTemplates.vue
+++ src/components/AdminSettings/GlobalTemplates.vue
@@ -1,3 +1,8 @@
+<!--
+  - SPDX-FileCopyrightText: 2024 Nextcloud GmbH and Nextcloud contributors
+  - SPDX-License-Identifier: AGPL-3.0-or-later
+-->
+
 <template>
 	<NcSettingsSection v-if="templatesAvailable"
 		:name="t('richdocuments', 'Global Templates')">
