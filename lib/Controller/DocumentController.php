13c6ae93d97d01e4eccb8bf8946e048d6556bb3f
Julius Knorr <jus@bitgrid.net>
2024-11-29 15:01:02 +0100

    tmp: NoCSRFRequired to move to OCS endpoint
    Signed-off-by: Julius Knorr <jus@bitgrid.net>

--- lib/Controller/DocumentController.php
+++ lib/Controller/DocumentController.php
@@ -387,6 +387,7 @@ class DocumentController extends Controller {
 	}
 
 	#[PublicPage]
+	#[NoCSRFRequired]
 	public function token(int $fileId, ?string $shareToken = null, ?string $path = null, ?string $guestName = null): DataResponse {
 		try {
 			$share = $shareToken ? $this->shareManager->getShareByToken($shareToken) : null;
