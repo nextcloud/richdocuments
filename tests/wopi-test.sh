90c9c430ad3d0aa8c805f80061556e5944e51e81
Julius Knorr <jus@bitgrid.net>
2024-11-29 15:26:46 +0100

    fixup! ci: Add ci job to run wopi-validator-core

--- tests/wopi-test.sh
+++ tests/wopi-test.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-
-set -e
-set -x
-
+# This script allows to run wopi-validator-core in a docker container
+# on a Nextcloud instance with the richdocuments app enabled
+# It is required to have a working WOPI server configured to generate a token
+#
+# For running locally as the docker container needs to be able to reach the Nextcloud instance
+# NEXTCLOUD_URL=http://nextcloud.local DOCKER_ARGS="--network nextcloud_default" HOST_IP=192.168.21.1 ./tests/wopi-test.sh
 
 # current timestamp as filename
 FILENAME=$(date +%s)
@@ -11,6 +13,7 @@ NEXTCLOUD_URL=${NEXTCLOUD_URL:-http://nextcloud.local}
 USERNAME=${USERNAME:-admin}
 PASSWORD=${PASSWORD:-admin}
 HOST_IP=${HOST_IP:-172.17.0.1}
+DOCKER_ARGS=${DOCKER_ARGS:-}
 
 curl $NEXTCLOUD_URL/status.php --max-time 5 --retry 5 --retry-delay 0 --retry-max-time 30 --retry-connrefused
 
@@ -26,7 +29,6 @@ CSRF_TOKEN=$(curl $NEXTCLOUD_URL/index.php/login --cookie-jar $COOKIEJAR -s | gr
 echo "File id: $FILE_ID"
 echo "CSRF token: $CSRF_TOKEN"
 
-
 WOPI_TOKEN=$(curl -u $USERNAME:$PASSWORD --cookie-jar $COOKIEJAR $NEXTCLOUD_URL/index.php/apps/richdocuments/token?fileId=$FILE_ID -X POST -H "requesttoken: $CSRF_TOKEN" -s | jq -r .token)
 
 # Nextcloud needed a valid mimetype before ut the wopi test can only be ran with a .wopitest file
@@ -37,4 +39,4 @@ WOPI_URL="$NEXTCLOUD_URL/index.php/apps/richdocuments/wopi/files/$FILE_ID"
 echo "WOPI URL: $WOPI_URL"
 echo "WOPI token generated: $WOPI_TOKEN"
 
-docker run --add-host nextcloud.local:${HOST_IP} --rm tylerbutler/wopi-validator -- -w $WOPI_URL -t $WOPI_TOKEN -l 0 "$@"
+docker run $DOCKER_ARGS --add-host "nextcloud.local:${HOST_IP}" --rm tylerbutler/wopi-validator -- -w $WOPI_URL -t $WOPI_TOKEN -l 0 "$@"
