241652c5595a6e339d29a9059aaf38601438d7f3
Julius Knorr <jus@bitgrid.net>
2024-12-27 12:25:19 +0100

    fix: Apply certificate check option as default request param
    Signed-off-by: Julius Knorr <jus@bitgrid.net>

--- lib/Service/RemoteOptionsService.php
+++ lib/Service/RemoteOptionsService.php
@@ -8,16 +8,23 @@ declare(strict_types=1);
 
 namespace OCA\Richdocuments\Service;
 
+use OCP\IAppConfig;
+
 class RemoteOptionsService {
 	public const REMOTE_TIMEOUT_DEFAULT = 5;
 
 	public static function getDefaultOptions(int $timeout = self::REMOTE_TIMEOUT_DEFAULT): array {
-		return [
+		$options = [
 			'timeout' => $timeout,
 			'nextcloud' => [
 				'allow_local_address' => true,
 			]
 		];
-	}
 
+		if (\OCP\Server::get(IAppConfig::class)->getValueString('richdocuments', 'disable_certificate_verification') === 'yes') {
+			$options['verify'] = false;
+		}
+
+		return $options;
+	}
 }
