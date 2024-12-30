da42ac47c13239934f6098c83ed5947138853334
Julius Knorr <jus@bitgrid.net>
2024-12-30 10:07:36 +0100

    chore: Update composer files with new composer version
    Signed-off-by: Julius Knorr <jus@bitgrid.net>

--- composer/composer/installed.php
+++ composer/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '19166b7157b61b5d23bebce5c61159b2f2f2a629',
+        'reference' => '594bb30d04cbfbf81dd495577656daf4c82cab87',
         'type' => 'library',
         'install_path' => __DIR__ . '/../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '19166b7157b61b5d23bebce5c61159b2f2f2a629',
+            'reference' => '594bb30d04cbfbf81dd495577656daf4c82cab87',
             'type' => 'library',
             'install_path' => __DIR__ . '/../',
             'aliases' => array(),
