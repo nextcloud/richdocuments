f24d64f23e2158a3b10a2e8e85bf3ad52fb5c04c
Elizabeth Danzberger <lizzy7128@tutanota.de>
2025-02-11 12:20:13 +0100

    refactor: move custom fonts into separate component
    Signed-off-by: Elizabeth Danzberger <lizzy7128@tutanota.de>

--- src/admin.js
+++ src/admin.js
@@ -5,6 +5,7 @@
 import './init-shared.js'
 import Vue from 'vue'
 import AdminSettings from './components/AdminSettings.vue'
+import { createPinia, PiniaVuePlugin } from 'pinia'
 import '../css/admin.scss'
 
 // CSP config for webpack dynamic chunk loading
@@ -21,9 +22,26 @@ Vue.prototype.n = n
 Vue.prototype.OC = OC
 Vue.prototype.OCA = OCA
 
+Vue.use(PiniaVuePlugin)
+
+const pinia = createPinia()
 const element = document.getElementById('admin-vue')
+const initialSettings = JSON.parse(element.dataset.initial)
+
+// Hydrate the adminSettings store with the initial settings
+// provided by the initial state
+pinia.state.value = {
+	adminSettings: {
+		fonts: initialSettings.fonts,
+	},
+}
 
 /* eslint-disable-next-line no-new */
 new Vue({
-	render: h => h(AdminSettings, { props: { initial: JSON.parse(element.dataset.initial) } }),
+	pinia,
+	render: h => h(
+		AdminSettings,
+		{
+			props: { initial: JSON.parse(element.dataset.initial) },
+		}),
 }).$mount('#admin-vue')
