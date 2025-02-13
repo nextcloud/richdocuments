e6818c91f577a4ea86a5d3981882b253468f8c85
Julius Knorr <jus@bitgrid.net>
2025-02-13 17:34:41 +0100

    feat: Add setup checks and verify WOPI connectivity
    Signed-off-by: Julius Knorr <jus@bitgrid.net>

--- lib/Controller/SettingsController.php
+++ lib/Controller/SettingsController.php
@@ -33,7 +33,6 @@ use OCP\IURLGenerator;
 use OCP\PreConditionNotMetException;
 use OCP\Util;
 use Psr\Log\LoggerInterface;
-use Symfony\Component\Console\Output\NullOutput;
 
 class SettingsController extends Controller {
 	// TODO adapt overview generation if we add more font mimetypes
@@ -69,9 +68,7 @@ class SettingsController extends Controller {
 
 	public function checkSettings(): DataResponse {
 		try {
-			$output = new NullOutput();
-			$this->connectivityService->testDiscovery($output);
-			$this->connectivityService->testCapabilities($output);
+			$this->connectivityService->test();
 		} catch (\Exception $e) {
 			$this->logger->error($e->getMessage(), ['exception' => $e]);
 			return new DataResponse([
@@ -184,9 +181,7 @@ class SettingsController extends Controller {
 		}
 
 		try {
-			$output = new NullOutput();
-			$this->connectivityService->testDiscovery($output);
-			$this->connectivityService->testCapabilities($output);
+			$this->connectivityService->test();
 			$this->connectivityService->autoConfigurePublicUrl();
 		} catch (\Throwable $e) {
 			return new JSONResponse([
