335ec07f0891ca59cd1977c14d836e54574bebc4
Julius Knorr <jus@bitgrid.net>
2025-02-14 00:31:05 +0100

    fixup! tmp: use single save us button - group seems to be broken again

--- cypress/e2e/integration.spec.js
+++ cypress/e2e/integration.spec.js
@@ -67,7 +67,7 @@ describe('Nextcloud integration', function() {
 		cy.get('@loleafletframe').within(() => {
 			cy.get('#File-tab-label').click()
 			cy.get('#file-saveas-button').click()
-			// cy.get('#saveas-entries #saveas-entry-1').click()
+			cy.get('#saveas-entries #saveas-entry-1').click()
 		})
 
 		cy.get('.saveas-dialog').should('be.visible')
