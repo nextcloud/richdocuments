ef68db3c633dc50a4a0da76ce99c042a299595b4
Elizabeth Danzberger <lizzy7128@tutanota.de>
2025-03-26 16:02:09 +0100

    fix(workflow): add trusted domain on CI
    Signed-off-by: Elizabeth Danzberger <lizzy7128@tutanota.de>

--- playwright/support/setup.ts
+++ playwright/support/setup.ts
@@ -4,7 +4,7 @@
  */
 
 import { test as setup } from '@playwright/test'
-import { configureNextcloud, runOcc } from '@nextcloud/e2e-test-server/docker'
+import { configureNextcloud, runOcc, setSystemConfig } from '@nextcloud/e2e-test-server/docker'
 
 /**
  * We use this to ensure Nextcloud is configured correctly before running our tests
@@ -15,6 +15,10 @@ import { configureNextcloud, runOcc } from '@nextcloud/e2e-test-server/docker'
 setup('configure nextcloud', async () => {
 	setup.slow()
 	await configureNextcloud(['richdocuments', 'testing'])
+
+	if (process.env.CI) {
+		await setSystemConfig('trusted_domains 1', '172.17.0.1')
+	}
 
 	await runOcc(['config:app:set', 'richdocuments', 'wopi_url', '--value', 'http://localhost:9980'])
 	await runOcc(['config:app:set', 'richdocuments', 'public_wopi_url', '--value', 'http://localhost:9980'])
