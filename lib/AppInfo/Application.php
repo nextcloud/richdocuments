1aa37bd6b5fb19df2de85ef44bb0230cab811e1b
Julien Veyssier <julien-nc@posteo.net>
2025-04-02 11:56:44 +0200

    feat: add docx generation tasktype+provider
    Signed-off-by: Julien Veyssier <julien-nc@posteo.net>

--- lib/AppInfo/Application.php
+++ lib/AppInfo/Application.php
@@ -32,6 +32,8 @@ use OCA\Richdocuments\Preview\OOXML;
 use OCA\Richdocuments\Preview\OpenDocument;
 use OCA\Richdocuments\Preview\Pdf;
 use OCA\Richdocuments\Reference\OfficeTargetReferenceProvider;
+use OCA\Richdocuments\TaskProcessing\TextToDocumentProvider;
+use OCA\Richdocuments\TaskProcessing\TextToDocumentTaskType;
 use OCA\Richdocuments\Template\CollaboraTemplateProvider;
 use OCA\Viewer\Event\LoadViewer;
 use OCP\AppFramework\App;
@@ -84,6 +86,8 @@ class Application extends App implements IBootstrap {
 		$context->registerPreviewProvider(Pdf::class, Pdf::MIMETYPE_REGEX);
 		$context->registerFileConversionProvider(ConversionProvider::class);
 		$context->registerNotifierService(Notifier::class);
+		$context->registerTaskProcessingTaskType(TextToDocumentTaskType::class);
+		$context->registerTaskProcessingProvider(TextToDocumentProvider::class);
 	}
 
 	public function boot(IBootContext $context): void {
---
+++ lib/TaskProcessing/TextToDocumentProvider.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+declare(strict_types=1);
+
+namespace OCA\Richdocuments\TaskProcessing;
+
+use OCA\Richdocuments\AppInfo\Application;
+use OCA\Richdocuments\Service\DocumentGenerationService;
+use OCP\IL10N;
+use OCP\TaskProcessing\ISynchronousProvider;
+
+class TextToDocumentProvider implements ISynchronousProvider {
+
+	public function __construct(
+		private DocumentGenerationService $documentGenerationService,
+		private IL10N $l10n,
+	) {
+	}
+
+	public function getId(): string {
+		return Application::APPNAME . '-slide_deck_generator';
+	}
+
+	public function getName(): string {
+		return $this->l10n->t('Nextcloud Assistant Slide Deck Generator');
+	}
+
+	public function getTaskTypeId(): string {
+		return TextToDocumentTaskType::ID;
+	}
+
+	public function getExpectedRuntime(): int {
+		return 120;
+	}
+
+	public function getInputShapeEnumValues(): array {
+		return [];
+	}
+
+	public function getInputShapeDefaults(): array {
+		return [];
+	}
+
+	public function getOptionalInputShape(): array {
+		return [];
+	}
+
+	public function getOptionalInputShapeEnumValues(): array {
+		return [];
+	}
+
+	public function getOptionalInputShapeDefaults(): array {
+		return [];
+	}
+
+	public function getOutputShapeEnumValues(): array {
+		return [];
+	}
+
+	public function getOptionalOutputShape(): array {
+		return [];
+	}
+
+	public function getOptionalOutputShapeEnumValues(): array {
+		return [];
+	}
+	/**
+	 * @inheritDoc
+	 */
+	public function process(?string $userId, array $input, callable $reportProgress): array {
+		if ($userId === null) {
+			throw new \RuntimeException('User ID is required to process the prompt.');
+		}
+
+		if (!isset($input['text']) || !is_string($input['text'])) {
+			throw new \RuntimeException('Invalid input, expected "text" key with string value');
+		}
+
+		$fileContent = $this->documentGenerationService->generateDocument(
+			$userId,
+			$input['text'],
+		);
+
+		error_log('BBBBBBBBBBBBBBB result ' . $fileContent);
+		return [
+			'file' => $fileContent,
+		];
+	}
+}
