80e5e3665574eecf5d039c6da030130969ce5560
Julius Knorr <jus@bitgrid.net>
2025-05-07 14:24:21 +0200

    fix(direct-editing): Use correct method to pass the Action_SaveAs post message
    Signed-off-by: Julius Knorr <jus@bitgrid.net>

--- src/document.js
+++ src/document.js
@@ -467,7 +467,7 @@ const documentsMain = {
 								path: documentsMain.fileName,
 								format: args.Format,
 							},
-							(value) => value && this.sendPostMessage('Action_SaveAs', { Filename: value, Notify: true }),
+							(value) => value && PostMessages.sendWOPIPostMessage('loolframe', 'Action_SaveAs', { Filename: value, Notify: true }),
 						)
 					} else if (msgId === 'Action_Save_Resp') {
 						if (args.success && args.fileName) {
