f1db58f794f1b019ccd8ab421880930c03d3b948
Franziska Bath <franziska.bath@strato.de>
2025-05-19 16:06:35 +0200

    IONOS: fix: for shares allow document creation only for users that can edit
    Signed-off-by: Franziska Bath <franziska.bath@strato.de>

--- src/public.js
+++ src/public.js
@@ -10,6 +10,7 @@ import {
 	isDownloadHidden,
 } from './helpers/index.js'
 import { getCapabilities } from './services/capabilities.ts'
+import { getCurrentUser } from '@nextcloud/auth'
 import NewFileMenu from './view/NewFileMenu.js'
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -17,7 +18,12 @@ document.addEventListener('DOMContentLoaded', () => {
 		return
 	}
 
-	if (OCA.Files && OCA.Files.fileActions) {
+	const userGroups = getCurrentUser()?.groups || [];
+	const editGroups = getCapabilities().config.edit_groups || [];
+	const editGroupsArray = Array.isArray(editGroups) ? editGroups : [editGroups];
+	const userInEditGroups = editGroupsArray.some(group => userGroups.includes(group));
+
+	if (OCA.Files && OCA.Files.fileActions && userInEditGroups) {
 		OC.Plugins.register('OCA.Files.NewFileMenu', NewFileMenu)
 	}
 
