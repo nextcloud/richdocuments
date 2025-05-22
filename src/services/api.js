5b25a975620a2c186425f34aac2281bfae3f5e2f
Julius Knorr <jus@bitgrid.net>
2025-05-22 15:38:41 +0200

    fix: Create new files on public pages for 31+
    Signed-off-by: Julius Knorr <jus@bitgrid.net>

--- src/services/api.js
+++ src/services/api.js
@@ -6,16 +6,14 @@
 import axios from '@nextcloud/axios'
 import { generateOcsUrl, generateFilePath } from '@nextcloud/router'
 import { getSharingToken } from '@nextcloud/sharing/public'
-import { getCurrentDirectory } from '../helpers/filesApp.js'
 
-export const createEmptyFile = async (mimeType, fileName, templateId = null) => {
+export const createEmptyFile = async (context, mimeType, fileName, templateId = null) => {
 	const shareToken = getSharingToken()
-	const directoryPath = getCurrentDirectory()
 
 	const response = await axios.post(generateOcsUrl('apps/richdocuments/api/v1/file', 2), {
 		mimeType,
 		fileName,
-		directoryPath,
+		directoryPath: context.dirname,
 		shareToken,
 		templateId,
 	})
