ade7eee2edec1eff5b1b98e24cc7870d9a500609
Luka Trovic <luka@nextcloud.com>
2025-09-04 21:24:05 +0200

    fix: backward compability
    Signed-off-by: Luka Trovic <luka@nextcloud.com>

--- src/viewer.js
+++ src/viewer.js
@@ -6,6 +6,27 @@
 import './init-shared.js'
 import '../css/filetypes.scss'
 
+import { getCapabilities } from './services/capabilities.ts'
 import { autoSetupBuiltInCodeServerIfNeeded } from './services/builtInCode.ts'
+
+// if this Nextcloud version ships a viewer with init script support, then we do not need to register the legacy viewer
+// but if no we need to register it:
+if (globalThis.OC.config.version < '30.0.14') {
+	const supportedMimes = getCapabilities().mimetypes
+	const AsyncViewerComponent = () => import('./view/Viewer.vue')
+
+	if (OCA.Viewer) {
+		OCA.Viewer.registerHandler({
+			id: 'richdocuments',
+			group: null,
+			mimes: supportedMimes,
+			component: AsyncViewerComponent,
+			theme: 'default',
+			canCompare: true,
+		})
+	} else {
+		console.error('Unable to register viewer handler')
+	}
+}
 
 autoSetupBuiltInCodeServerIfNeeded()
