5dc15a93b41cafa440366629c83b74ed6dddfa39
Luka Trovic <luka@nextcloud.com>
2025-09-05 20:17:09 +0200

    fix: update version check
    Signed-off-by: Luka Trovic <luka@nextcloud.com>

--- src/viewer.js
+++ src/viewer.js
@@ -5,13 +5,14 @@
 
 import './init-shared.js'
 import '../css/filetypes.scss'
+import semver from 'semver'
 
 import { getCapabilities } from './services/capabilities.ts'
 import { autoSetupBuiltInCodeServerIfNeeded } from './services/builtInCode.ts'
 
 // if this Nextcloud version ships a viewer with init script support, then we do not need to register the legacy viewer
 // but if no we need to register it:
-if (globalThis.OC.config.version < '30.0.14') {
+if (semver.lt(semver.coerce(globalThis.OC.config.version), '30.0.15')) {
 	const supportedMimes = getCapabilities().mimetypes
 	const AsyncViewerComponent = () => import('./view/Viewer.vue')
 
