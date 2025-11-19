3b2487997ad78e1f66b52399a01f632dd6fac3a9
Elizabeth Danzberger <elizabeth@elzody.dev>
2025-11-19 15:00:23 -0500

    feat(test): add test coverage
    Signed-off-by: Elizabeth Danzberger <elizabeth@elzody.dev>

--- cypress/e2e/new.spec.js
+++ cypress/e2e/new.spec.js
@@ -2,8 +2,8 @@
  * SPDX-FileCopyrightText: 2023 Julius Härtl <jus@bitgrid.net>
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
-// FIXME: Re-renable once 28 has file creation again working
-describe.skip('Create new office files', function() {
+
+describe('New file menu', function() {
 
 	let randUser
 	before(function() {
@@ -18,51 +18,34 @@ describe.skip('Create new office files', function() {
 	})
 
 	it('Shows create file entries', function() {
-		cy.get('.files-controls .button.new')
+		cy.get('form[data-cy-upload-picker=""]')
 			.should('be.visible')
 			.click()
 
-		cy.get('.newFileMenu', { timeout: 10000 })
+		cy.get('button[role="menuitem"]')
+			.contains('New document')
 			.should('be.visible')
-			.contains('.menuitem', 'New document')
-			.should('be.visible')
-			.find('.icon')
-			.should('have.css', 'background-image')
 
-		cy.get('.files-controls .button.new')
+		cy.get('form[data-cy-upload-picker=""]')
 			.click()
 
-		cy.get('.newFileMenu', { timeout: 10000 })
+		cy.get('li[data-cy-upload-picker-menu-entry="upload-file"]')
 			.should('not.be.visible')
 	})
 
-	const newFileTypeLabels = [
-		'document', 'spreadsheet', 'presentation', 'diagram',
-	]
-	newFileTypeLabels.forEach((filetype) => {
-		it('Create empty ' + filetype + ' file', function() {
-			cy.get('.files-controls .button.new')
-				.should('be.visible')
-				.click()
+	describe('Creates a new file', function() {
+		const newFileTypeLabels = [
+			'document', 'spreadsheet', 'presentation', 'diagram',
+		]
+		newFileTypeLabels.forEach((filetype) => {
+			it('Create empty ' + filetype + ' file', function() {
+				cy.newFileFromMenu(filetype, 'MyNewFile')
+				cy.waitForViewer()
+				cy.waitForCollabora()
 
-			cy.get('.newFileMenu', { timeout: 10000 })
-				.should('be.visible')
-				.contains('.menuitem', 'New ' + filetype)
-				.as('menuitem')
-				.should('be.visible')
-				.click()
+				cy.screenshot('new-file-' + filetype)
 
-			cy.get('@menuitem').find('.filenameform input[type=text]').type('MyNewFile')
-			cy.get('@menuitem').find('.filenameform .icon-confirm').click()
-
-			cy.waitForViewer()
-			cy.waitForCollabora()
-
-			cy.screenshot('new-file-' + filetype)
-
-			cy.get('@loleafletframe').within(() => {
-				cy.get('#closebutton').click()
-				cy.waitForViewerClose()
+				cy.closeDocument()
 			})
 		})
 	})
