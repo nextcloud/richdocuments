6b2d0bb2515f4e29e57bf92a22129c67a32105ce
Elizabeth Danzberger <elizabeth@elzody.dev>
2025-11-19 12:40:11 -0500

    chore: remove new file entry workaround
    Signed-off-by: Elizabeth Danzberger <elizabeth@elzody.dev>

--- src/services/api.js
+++ src/services/api.js
@@ -4,22 +4,7 @@
  */
 
 import axios from '@nextcloud/axios'
-import { generateOcsUrl, generateFilePath } from '@nextcloud/router'
-import { getSharingToken } from '@nextcloud/sharing/public'
-
-export const createEmptyFile = async (context, mimeType, fileName, templateId = null) => {
-	const shareToken = getSharingToken()
-
-	const response = await axios.post(generateOcsUrl('apps/richdocuments/api/v1/file', 2), {
-		mimeType,
-		fileName,
-		directoryPath: context.dirname,
-		shareToken,
-		templateId,
-	})
-
-	return response.data
-}
+import { generateFilePath } from '@nextcloud/router'
 
 export const savePersonalSetting = (data) => {
 	return axios.post(generateFilePath('richdocuments', 'ajax', 'personal.php'), data)
