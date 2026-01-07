c63e0c34b6896a05d23e35f052bdab7f0213c645
Elizabeth Danzberger <elizabeth@elzody.dev>
2026-01-07 18:44:37 -0500

    fix: only let admins request admin settings
    Signed-off-by: Elizabeth Danzberger <elizabeth@elzody.dev>

--- lib/Controller/SettingsController.php
+++ lib/Controller/SettingsController.php
@@ -100,7 +100,6 @@ class SettingsController extends Controller {
 		return new DataResponse([], Http::STATUS_NOT_FOUND);
 	}
 
-	#[NoAdminRequired]
 	public function getSettings(): JSONResponse {
 		return new JSONResponse($this->getSettingsData());
 	}
