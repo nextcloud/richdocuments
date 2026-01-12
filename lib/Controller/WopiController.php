cd28335e3e0fc17f3715371d11ee3b18f811fe03
Elizabeth Danzberger <elizabeth@elzody.dev>
2026-01-12 12:17:29 -0500

    fix: check admin before file read
    Signed-off-by: Elizabeth Danzberger <elizabeth@elzody.dev>

--- lib/Controller/WopiController.php
+++ lib/Controller/WopiController.php
@@ -448,6 +448,13 @@ class WopiController extends Controller {
 				throw new \Exception('UserID is empty');
 			}
 
+			$isUserAdmin = $this->groupManager->isAdmin($userId);
+			// Use the fileId as a file path URL (e.g., "/settings/systemconfig/wordbook/en_US%20(1).dic")
+			$settingsUrl = new SettingsUrl($fileId);
+			if ($settingsUrl->isSystemConfig() && !$isUserAdmin) {
+				throw new NotPermittedException();
+			}
+
 			$content = fopen('php://input', 'rb');
 			if (!$content) {
 				throw new \Exception('Failed to read input stream.');
@@ -456,12 +463,6 @@ class WopiController extends Controller {
 			$fileContent = stream_get_contents($content);
 			fclose($content);
 
-			$isUserAdmin = $this->groupManager->isAdmin($userId);
-			// Use the fileId as a file path URL (e.g., "/settings/systemconfig/wordbook/en_US%20(1).dic")
-			$settingsUrl = new SettingsUrl($fileId);
-			if ($settingsUrl->isSystemConfig() && !$isUserAdmin) {
-				throw new NotPermittedException();
-			}
 
 			$result = $this->settingsService->uploadFile($settingsUrl, $fileContent, $userId);
 
