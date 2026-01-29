135e195ca14c268aa2a239b0fad97c0ae4fb3436
Julius Knorr <jus@bitgrid.net>
2026-01-29 22:30:46 +0100

    chore: Remove unused views logic
    Signed-off-by: Julius Knorr <jus@bitgrid.net>

--- src/view/FilesAppIntegration.js
+++ src/view/FilesAppIntegration.js
@@ -28,17 +28,11 @@ export default {
 
 	filePath: undefined,
 
-	/* Views: people currently editing the file */
-	views: {},
-
 	followingEditor: false,
 
 	following: null,
 
 	handlers: {},
-
-	startLoading() {
-	},
 
 	init({ fileName, fileId, filePath, sendPostMessage }) {
 		this.fileNode = undefined
@@ -158,29 +152,6 @@ export default {
 
 	insertFile(mimeTypeFilter, insertFileProc) {
 		this.insertFile_impl(mimeTypeFilter, insertFileProc, this.handlers.insertFile)
-	},
-
-	setViews(views) {
-		this.views = {}
-		views.forEach((view) => {
-			this.views[view.ViewId] = view
-		})
-	},
-
-	followReset(event) {
-		this.sendPostMessage('Action_FollowUser', { Follow: false })
-		this.following = null
-		this.followingEditor = false
-	},
-	followCurrentEditor(event) {
-		this.sendPostMessage('Action_FollowUser', { Follow: true })
-		this.following = null
-		this.followingEditor = true
-	},
-	followView(view) {
-		this.sendPostMessage('Action_FollowUser', { ViewId: view.ViewId, Follow: true })
-		this.following = view.ViewId
-		this.followingEditor = false
 	},
 
 	async showRevHistory() {
