5d190e46a22897607c2ba6e2d8e3203ba3ed94b9
Elizabeth Danzberger <elizabeth@elzody.dev>
2026-05-08 15:07:26 -0400

    test: add overview page tests
    Adds some basic Cypress and PHP unit tests to have some tests in place already
    
    Signed-off-by: Elizabeth Danzberger <elizabeth@elzody.dev>

---
+++ tests/lib/Controller/OverviewControllerTest.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * SPDX-FileCopyrightText: 2026 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+namespace Tests\Richdocuments\Controller;
+
+use OCA\Richdocuments\Controller\OverviewController;
+use OCP\AppFramework\Http\TemplateResponse;
+use OCP\IRequest;
+use Test\TestCase;
+
+class OverviewControllerTest extends TestCase {
+
+	public function testIndexReturnsTemplateResponse(): void {
+		$request = $this->createMock(IRequest::class);
+		$controller = new OverviewController('richdocuments', $request);
+		$response = $controller->index();
+
+		$this->assertInstanceOf(TemplateResponse::class, $response);
+		$this->assertSame('richdocuments', $response->getApp());
+		$this->assertSame('overview', $response->getTemplateName());
+		$this->assertSame('#app-content-vue', $response->getParams()['id-app-content']);
+		$this->assertSame('#app-navigation-vue', $response->getParams()['id-app-navigation']);
+	}
+}
