9d6a42d65a31f9ec3fda47c433a283f83946a9fe
Elizabeth Danzberger <elizabeth@elzody.dev>
2026-05-14 13:59:35 -0400

    test(overview): preview tests
    Add Cypress assertion to check if the preview exists, and a PHP unit test to make sure the initial state is set correctly.
    
    Signed-off-by: Elizabeth Danzberger <elizabeth@elzody.dev>

--- tests/lib/Controller/OverviewControllerTest.php
+++ tests/lib/Controller/OverviewControllerTest.php
@@ -10,7 +10,9 @@ namespace Tests\Richdocuments\Controller;
 
 use OCA\Richdocuments\Controller\OverviewController;
 use OCP\AppFramework\Http\TemplateResponse;
+use OCP\AppFramework\Services\IInitialState;
 use OCP\EventDispatcher\IEventDispatcher;
+use OCP\IPreview;
 use OCP\IRequest;
 use Test\TestCase;
 
@@ -19,8 +21,11 @@ class OverviewControllerTest extends TestCase {
 	public function testIndexReturnsTemplateResponse(): void {
 		$request = $this->createMock(IRequest::class);
 		$eventDispatcher = $this->createMock(IEventDispatcher::class);
+		$initialState = $this->createMock(IInitialState::class);
+		$preview = $this->createMock(IPreview::class);
+		$preview->method('isMimeSupported')->willReturn(false);
 
-		$controller = new OverviewController('richdocuments', $request, $eventDispatcher);
+		$controller = new OverviewController('richdocuments', $request, $eventDispatcher, $initialState, $preview);
 		$response = $controller->index();
 
 		$this->assertInstanceOf(TemplateResponse::class, $response);
@@ -28,5 +33,43 @@ class OverviewControllerTest extends TestCase {
 		$this->assertSame('overview', $response->getTemplateName());
 		$this->assertSame('#app-content-vue', $response->getParams()['id-app-content']);
 		$this->assertSame('#app-navigation-vue', $response->getParams()['id-app-navigation']);
+	}
+
+	public function testIndexSetsPreviewEnabledTrue(): void {
+		$request = $this->createMock(IRequest::class);
+		$eventDispatcher = $this->createMock(IEventDispatcher::class);
+		$initialState = $this->createMock(IInitialState::class);
+		$preview = $this->createMock(IPreview::class);
+
+		$preview->expects($this->once())
+			->method('isMimeSupported')
+			->with('application/vnd.oasis.opendocument.text')
+			->willReturn(true);
+
+		$initialState->expects($this->once())
+			->method('provideInitialState')
+			->with('previewEnabled', true);
+
+		$controller = new OverviewController('richdocuments', $request, $eventDispatcher, $initialState, $preview);
+		$controller->index();
+	}
+
+	public function testIndexSetsPreviewEnabledFalse(): void {
+		$request = $this->createMock(IRequest::class);
+		$eventDispatcher = $this->createMock(IEventDispatcher::class);
+		$initialState = $this->createMock(IInitialState::class);
+		$preview = $this->createMock(IPreview::class);
+
+		$preview->expects($this->once())
+			->method('isMimeSupported')
+			->with('application/vnd.oasis.opendocument.text')
+			->willReturn(false);
+
+		$initialState->expects($this->once())
+			->method('provideInitialState')
+			->with('previewEnabled', false);
+
+		$controller = new OverviewController('richdocuments', $request, $eventDispatcher, $initialState, $preview);
+		$controller->index();
 	}
 }
