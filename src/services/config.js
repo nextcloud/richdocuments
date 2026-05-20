ac64bf2c7b3030f591e2f6557efb5bb49f76f154
James Manuel <moodyjmz@users.noreply.github.com>
2026-05-21 00:06:36 +0200

    fix(overview): use FileDocumentOutline as grid preview fallback; add JSDoc to setOverviewGridView
    Signed-off-by: James Manuel <moodyjmz@users.noreply.github.com>

--- src/services/config.js
+++ src/services/config.js
@@ -6,6 +6,12 @@
 import axios from '@nextcloud/axios'
 import { generateUrl } from '@nextcloud/router'
 
+/**
+ * Persist the user's grid/list view preference for the Office Overview.
+ *
+ * @param {boolean} value True for grid view, false for list view
+ * @return {Promise<void>}
+ */
 export async function setOverviewGridView(value) {
 	await axios.put(generateUrl('/apps/richdocuments/settings/overview/grid_view'), { value })
 }
